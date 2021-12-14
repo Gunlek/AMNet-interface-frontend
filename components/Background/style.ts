87fe985994b93b353a3e505a583eb47106c1a808
Argilla <gauthier.pailhas@gmail.com>
2021-12-14 19:36:32 +0100

    :pencil2: (Static) Normalization name file

--- components/Background/style.ts
+++ components/Background/style.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 export const CampusBackground = styled.div`
   min-height: 100vh;
-  background-image: url("/static/images/background_tbk.png");
+  background-image: url("/static/images/campus.png");
   background-repeat: no-repeat;
   background-size: cover;
   background-attachment: fixed;
 `;
 
 export const DefaultBackground = styled.div`
-  height: 100%;
+  min-height: 100vh;
   background: #E5E5E5;
 `;
 
