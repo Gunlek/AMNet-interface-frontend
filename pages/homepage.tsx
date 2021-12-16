236ac46d6025ebf9e0dd1890093fc0c406e99243
Argilla <gauthier.pailhas@gmail.com>
2021-12-16 18:51:06 +0100

    :sparkles: (Homepage) Add Minecraft card

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -1,53 +1,57 @@
 import React from "react";
+import Head from "next/head";
 import {CampusBackground} from "../components/Background/style";
-import {HelpSection, Col, Card, RectangleLogo} from "../components/Card/Cards";
+import {HelpSection, RectangleLogo , TeamPicture} from "../components/Card/Cards";
 import {GreenButton} from "../components/Button/Buttons";
-import {StyledFlexDivCol, StyledTeamPicture, StyledFlexDivRow, StyledWhiteTxt, StyledGreenText, StyledGreenLine,StyledGreenCard} from "../components/Card/style";
+import {StyledWhiteTxt, StyledGreenText, StyledGreenLine, StyledCard, StyledBlackText} from "../components/Card/style";
+import {StyledFlexCol , StyledFlexRow, StyledFlexDiv} from "../components/Container/style";
 
 export default function Homepage() {
   return (
     <>
+      <Head><title>Acceuil &bull; AMNet</title></Head>
       <CampusBackground>
-        <StyledFlexDivCol margin="0" padding="0.5%">
-          <StyledFlexDivRow>
-            <Col><RectangleLogo></RectangleLogo></Col>
-            <Col><StyledFlexDivCol align="end"><GreenButton size="44%">S'inscrire / Se Connecter</GreenButton></StyledFlexDivCol></Col>
-          </StyledFlexDivRow>
-          <StyledFlexDivRow>
-            <Col><StyledWhiteTxt>L’AMNet est une association gérée par les étudiants qui a pour but d’administrer le réseau internet de la résidence Jacques Pagliero. Elle est totalement indépendante de l’administration de la résidence ou de l’école.<br></br><br></br> Votre cotisation sert à améliorer l’installation ainsi qu’à payer les abonnements internet. Chaque année, 80% des cotisations est utilisé directement et les 20% restants servent à créer une trésorerie pour des investissements futurs. </StyledWhiteTxt></Col>
-            <Col>
-              <StyledTeamPicture>
-                <StyledFlexDivCol>
-                  <StyledFlexDivCol width="100%" height="50%" justify="start" align="start">
-                    <StyledGreenCard>La Team qui gère l’AMNet pour l’année 2021-2022</StyledGreenCard>
-                  </StyledFlexDivCol>
-                  <StyledFlexDivCol width="100%" height="50%" margin="0" justify="end" margin="0">
-                      <Card width="100%" radius="20px">
-                        <StyledFlexDivRow width="100%">
-                          <Col colnumber="3" width="33%"><StyledFlexDivCol>Sdoosh <StyledGreenText>96Li220</StyledGreenText></StyledFlexDivCol></Col>
-                          <Col colnumber="3" width="33%"><StyledFlexDivCol>Nem'O <StyledGreenText>74Li220</StyledGreenText></StyledFlexDivCol></Col>
-                          <Col colnumber="3" width="33%"><StyledFlexDivCol>Trobotyk'ss (ML)° <StyledGreenText>47Li220</StyledGreenText></StyledFlexDivCol></Col>
-                        </StyledFlexDivRow>
-                      </Card>
-                  </StyledFlexDivCol>
-                </StyledFlexDivCol>
-              </StyledTeamPicture>
-            </Col>
-          </StyledFlexDivRow>
-          <Card width="90%" height="25%">
-            <StyledFlexDivRow width="100%">
-              <Col width="15%"><StyledFlexDivCol><img height="90%" src="/static/images/mc_logo.png" alt="Logo Minecraft" /></StyledFlexDivCol></Col>
-              <Col width="85%">
-                <StyledFlexDivRow width="100%">
-                  <Col width="20%"><StyledGreenText>Serveur Minecraft</StyledGreenText></Col>
-                  <Col width="80%"><StyledGreenLine></StyledGreenLine></Col>
-                </StyledFlexDivRow>
-                Nous proposons également d’autres services comme un serveur Minecraft : minecraft.amnet.fr
-              </Col>
-            </StyledFlexDivRow>  
-          </Card>
-          <HelpSection></HelpSection>
-        </StyledFlexDivCol>
+        <StyledFlexCol justify="space-around">
+          <StyledFlexRow  width="90%" height="15%">
+            <StyledFlexDiv grow="2">
+              <StyledFlexRow justify="start"><RectangleLogo color="blanc"></RectangleLogo></StyledFlexRow>
+            </StyledFlexDiv>
+            <StyledFlexDiv grow="2">
+              <StyledFlexRow justify="end"><GreenButton size="44%">S'inscrire / Se Connecter</GreenButton></StyledFlexRow>
+            </StyledFlexDiv>
+          </StyledFlexRow>   
+          <StyledFlexRow width="90%" height="50%">
+              <StyledFlexDiv grow ="2" width="45%">
+                <StyledWhiteTxt>L’AMNet est une association gérée par les étudiants qui a pour but d’administrer le réseau internet de  la résidence Jacques Pagliero. Elle est totalement indépendante de l’administration de la résidence ou de l’école.<br></br><br></br>Votre cotisation sert à améliorer l’installation ainsi qu’à payer les abonnements internet. Chaque année, 80% des cotisations est utilisé directement et les 20% restants servent à créer une trésorerie pour des investissements futurs. 
+                </StyledWhiteTxt>
+              </StyledFlexDiv>
+              <StyledFlexDiv grow ="2">
+                <TeamPicture names="Trobotyk'ss(ML)°;Sdoosh;Nem'O" nums="47Li220;96Li220;74Li220"></TeamPicture>
+              </StyledFlexDiv>
+            </StyledFlexRow> 
+          <StyledCard width="90%" height="20%" minheight="170px">
+            <StyledFlexRow> 
+              <StyledFlexCol width="250px">
+                <img style={{height: "80%", aspectRatio: "1 / 1"}} src="/static/logo/mc_logo.png" alt="Logo Minecraft" />
+              </StyledFlexCol>
+              <StyledFlexCol width="calc(100% - 250px)">
+                <StyledFlexRow height="50px" width="100%" justify="start">
+                  <StyledFlexDiv width="200px"><StyledGreenText lineheight="50px">Serveur Minecraft</StyledGreenText></StyledFlexDiv>
+                  <StyledFlexDiv  width="calc(100% - 200px)" height="0px"><StyledGreenLine></StyledGreenLine></StyledFlexDiv>
+                </StyledFlexRow>
+                <StyledFlexDiv width="100%"> 
+                  <StyledBlackText fontsize="1.2rem">
+                    En plus de fournir un accès internet aux résidents nous offrons une multitude de services, un serveur Minecraft : <span style={{color: "#096A09"}}>minecraft.amnet.fr</span> et d'autres que nous vous laisserons découvrir un jour ...
+                    <br></br><br></br>
+                    Si vous avez des idées de services que nous pourrions proposer hésitez pas à nous contacter !
+                  </StyledBlackText>
+                  <StyledBlackText fontsize="1.2rem"></StyledBlackText>
+                </StyledFlexDiv>
+              </StyledFlexCol>
+            </StyledFlexRow>
+          </StyledCard>
+          <StyledFlexRow width="90%" height="1.1rem"><HelpSection></HelpSection></StyledFlexRow>   
+        </StyledFlexCol>
       </CampusBackground>
     </>
   );
