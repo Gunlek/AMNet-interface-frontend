2fc2d1bf990c417529ddd9403ffd520c938a9a23
Fabien Aubret <fabien.aubret@gmail.com>
2021-12-19 18:03:24 +0100

    :recycle: (Buttons) Add ButtonProps type

--- components/Button/Buttons.tsx
+++ components/Button/Buttons.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { StyledGreenButton, StyledRedButton } from "./style";
 
-export function GreenButton(props: { children: string, size?: string }) {
-  return <StyledGreenButton size={props.size}>{props.children}</StyledGreenButton>;
+type ButtonProps = { children: React.ReactNode; size?: string };
+
+export function GreenButton(props: ButtonProps) {
+  return (
+    <StyledGreenButton size={props.size}>{props.children}</StyledGreenButton>
+  );
 }
 
-export function RedButton(props: { children: string, size?: string }) {
+export function RedButton(props: ButtonProps) {
   return <StyledRedButton size={props.size}>{props.children}</StyledRedButton>;
 }
