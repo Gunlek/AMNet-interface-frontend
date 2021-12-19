a5ae8ea9fdaf77632a4c1146eff3a3e811e06079
Fabien Aubret <fabien.aubret@gmail.com>
2021-12-19 18:08:49 +0100

    :recycle: (StyledText) Update css

--- components/Card/style.ts
+++ components/Card/style.ts
@@ -1,69 +1,68 @@
 import styled from "styled-components";
-import {StyledFlexCol, StyledFlexDiv} from "../Container/style";
+import { StyledFlexCol, StyledFlexDiv } from "../Container/style";
 
 export const StyledCard = styled(StyledFlexDiv)`
-    background: rgba(255, 255, 255, 0.9);
-    box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.06);
-    border-radius: ${props=>props.radius || '30px'};
-    min-height: ${props=>props.minheight};
+  background: rgba(255, 255, 255, 0.9);
+  box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.06);
+  border-radius: ${(props) => props.radius || "30px"};
+  min-height: ${(props) => props.minheight};
 `;
 
 export const StyledGreenCard = styled.div`
-    background: #096A09;
-    border-radius: 13px 20px 20px 0px;
-    color: #FFFFFF;
-    width: 99%;
-    padding-left:47px;
-    height:2.2rem;
-    line-height: 2.2rem;
-    font-size: 1.1rem;
+  background: #096a09;
+  border-radius: 13px 20px 20px 0px;
+  color: #ffffff;
+  width: 99%;
+  padding-left: 47px;
+  height: 2.2rem;
+  line-height: 2.2rem;
+  font-size: 1.1rem;
 `;
 
 export const StyledInput = styled(StyledCard)`
-    background: rgba(255, 255, 255, 0.6);
-    border-radius: 20px;
+  background: rgba(255, 255, 255, 0.6);
+  border-radius: 20px;
 `;
 
 export const StyledRadio = styled.input`
-    width: 34px;
-    height: 34px;
-    border: 4px solid #096A09;
-    box-sizing: border-box;
-    border-radius: 9px;
+  width: 34px;
+  height: 34px;
+  border: 4px solid #096a09;
+  box-sizing: border-box;
+  border-radius: 9px;
 `;
 
 export const StyledTeamPicture = styled(StyledFlexCol)`
-    background-image: url("/static/images/team.png");
-    background-repeat: no-repeat;
-    background-size: cover;
-    height: 100%;
-    width: 100%;
-    border-radius: 30px;
+  background-image: url("/static/images/team.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100%;
+  width: 100%;
+  border-radius: 30px;
 `;
 
-export const StyledHelpSection= styled.div`
-    color: ${props=>props.color || '#FFFFFF'};
-    text-align: center;
-    font-size: 1.1rem
+export const StyledHelpSection = styled.div`
+  color: ${(props) => props.color || "#FFFFFF"};
+  text-align: center;
+  font-size: 1.1rem;
 `;
 
-export const StyledWhiteTxt= styled.div`
-    color: white;
-    font-size: ${props=>props.fontsize || '1.4rem'};
-    line-height: ${props=>props.lineheight};
+export const StyledWhiteText = styled.div`
+  color: white;
+  font-size: 1.2em;
 `;
 
-export const StyledGreenText = styled(StyledWhiteTxt)`
-    color: #096A09;
+export const StyledGreenText = styled(StyledWhiteText)`
+  color: #096a09;
 `;
 
-export const StyledBlackText = styled(StyledWhiteTxt)`
-    color: black;
+export const StyledBlackText = styled(StyledWhiteText)`
+  color: black;
 `;
 
 export const StyledGreenLine = styled.div`
-    width: 95%;
-    height: 2px;
-    background: #096A09;
-    margin-left:20px;
+  width: 95%;
+  height: 2px;
+  background: #096a09;
+  margin-left: 20px;
 `;
--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -1,56 +1,102 @@
 import React from "react";
 import Head from "next/head";
-import {CampusBackground} from "../components/Background/style";
-import {HelpSection, RectangleLogo , TeamPicture} from "../components/Card/Cards";
-import {GreenButton} from "../components/Button/Buttons";
-import {StyledWhiteTxt, StyledGreenText, StyledGreenLine, StyledCard, StyledBlackText} from "../components/Card/style";
-import {StyledFlexCol , StyledFlexRow, StyledFlexDiv} from "../components/Container/style";
+import { CampusBackground } from "../components/Background/style";
+import {
+  HelpSection,
+  RectangleLogo,
+  TeamPicture,
+} from "../components/Card/Cards";
+import { GreenButton } from "../components/Button/Buttons";
+import {
+  StyledWhiteText,
+  StyledGreenText,
+  StyledGreenLine,
+  StyledCard,
+  StyledBlackText,
+} from "../components/Card/style";
+import {
+  StyledFlexCol,
+  StyledFlexRow,
+  StyledFlexDiv,
+} from "../components/Container/style";
 
 export default function Homepage() {
   return (
     <>
-      <Head><title>Acceuil &bull; AMNet</title></Head>
+      <Head>
+        <title>Accueil &bull; AMNet</title>
+      </Head>
       <CampusBackground>
         <StyledFlexCol justify="space-around">
-          <StyledFlexRow  width="90%" height="15%">
+          <StyledFlexRow width="90%" height="15%">
             <StyledFlexDiv grow="2">
-              <StyledFlexRow justify="start"><RectangleLogo color="blanc"></RectangleLogo></StyledFlexRow>
+              <StyledFlexRow justify="start">
+                <RectangleLogo color="blanc"></RectangleLogo>
+              </StyledFlexRow>
             </StyledFlexDiv>
             <StyledFlexDiv grow="2">
-              <StyledFlexRow justify="end"><GreenButton size="44%">S'inscrire / Se Connecter</GreenButton></StyledFlexRow>
+              <StyledFlexRow justify="end">
+                <GreenButton size="44%">S'inscrire / Se Connecter</GreenButton>
+              </StyledFlexRow>
             </StyledFlexDiv>
-          </StyledFlexRow>   
+          </StyledFlexRow>
           <StyledFlexRow width="90%" height="50%">
-              <StyledFlexDiv grow ="2" width="45%">
-                <StyledWhiteTxt>L’AMNet est une association gérée par les étudiants qui a pour but d’administrer le réseau internet de  la résidence Jacques Pagliero. Elle est totalement indépendante de l’administration de la résidence ou de l’école.<br></br><br></br>Votre cotisation sert à améliorer l’installation ainsi qu’à payer les abonnements internet. Chaque année, 80% des cotisations est utilisé directement et les 20% restants servent à créer une trésorerie pour des investissements futurs. 
-                </StyledWhiteTxt>
-              </StyledFlexDiv>
-              <StyledFlexDiv grow ="2">
-                <TeamPicture names="Trobotyk'ss(ML)°;Sdoosh;Nem'O" nums="47Li220;96Li220;74Li220"></TeamPicture>
-              </StyledFlexDiv>
-            </StyledFlexRow> 
+            <StyledFlexDiv grow="2" width="45%">
+              <StyledWhiteText>
+                L’AMNet est une association gérée par les étudiants qui a pour
+                but d’administrer le réseau internet de la résidence Jacques
+                Pagliero. Elle est totalement indépendante de l’administration
+                de la résidence ou de l’école.<br></br>
+                <br></br>Votre cotisation sert à améliorer l’installation ainsi
+                qu’à payer les abonnements internet. Chaque année, 80% des
+                cotisations est utilisé directement et les 20% restants servent
+                à créer une trésorerie pour des investissements futurs.
+              </StyledWhiteText>
+            </StyledFlexDiv>
+            <StyledFlexDiv grow="2">
+              <TeamPicture
+                names="Trobotyk'ss(ML)°;Sdoosh;Nem'O"
+                nums="47Li220;96Li220;74Li220"
+              ></TeamPicture>
+            </StyledFlexDiv>
+          </StyledFlexRow>
           <StyledCard width="90%" height="20%" minheight="170px">
-            <StyledFlexRow> 
+            <StyledFlexRow>
               <StyledFlexCol width="250px">
-                <img style={{height: "80%", aspectRatio: "1 / 1"}} src="/static/logo/mc_logo.png" alt="Logo Minecraft" />
+                <img
+                  style={{ height: "80%", aspectRatio: "1 / 1" }}
+                  src="/static/logo/mc_logo.png"
+                  alt="Logo Minecraft"
+                />
               </StyledFlexCol>
               <StyledFlexCol width="calc(100% - 250px)">
                 <StyledFlexRow height="50px" width="100%" justify="start">
-                  <StyledFlexDiv width="200px"><StyledGreenText lineheight="50px">Serveur Minecraft</StyledGreenText></StyledFlexDiv>
-                  <StyledFlexDiv  width="calc(100% - 200px)" height="0px"><StyledGreenLine></StyledGreenLine></StyledFlexDiv>
+                  <StyledFlexDiv width="200px">
+                    <StyledGreenText>Serveur Minecraft</StyledGreenText>
+                  </StyledFlexDiv>
+                  <StyledFlexDiv width="calc(100% - 200px)" height="0px">
+                    <StyledGreenLine></StyledGreenLine>
+                  </StyledFlexDiv>
                 </StyledFlexRow>
-                <StyledFlexDiv width="100%"> 
-                  <StyledBlackText fontsize="1.2rem">
-                    En plus de fournir un accès internet aux résidents nous offrons une multitude de services, un serveur Minecraft : <span style={{color: "#096A09"}}>minecraft.amnet.fr</span> et d'autres que nous vous laisserons découvrir un jour ...
-                    <br></br><br></br>
-                    Si vous avez des idées de services que nous pourrions proposer hésitez pas à nous contacter !
+                <StyledFlexDiv width="100%">
+                  <StyledBlackText>
+                    En plus de fournir un accès internet aux résidents nous
+                    offrons une multitude de services, un serveur Minecraft :{" "}
+                    <span style={{ color: "#096A09" }}>minecraft.amnet.fr</span>{" "}
+                    et d'autres que nous vous laisserons découvrir un jour ...
+                    <br></br>
+                    <br></br>
+                    Si vous avez des idées de services que nous pourrions
+                    proposer hésitez pas à nous contacter !
                   </StyledBlackText>
-                  <StyledBlackText fontsize="1.2rem"></StyledBlackText>
+                  <StyledBlackText></StyledBlackText>
                 </StyledFlexDiv>
               </StyledFlexCol>
             </StyledFlexRow>
           </StyledCard>
-          <StyledFlexRow width="90%" height="1.1rem"><HelpSection></HelpSection></StyledFlexRow>   
+          <StyledFlexRow width="90%" height="1.1rem">
+            <HelpSection></HelpSection>
+          </StyledFlexRow>
         </StyledFlexCol>
       </CampusBackground>
     </>
