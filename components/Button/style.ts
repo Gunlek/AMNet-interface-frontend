6234432ca77a060b30cb2c730b83e2b4edd91710
Fabien Aubret <fabien.aubret@gmail.com>
2021-12-19 18:03:40 +0100

    :recycle: (Constants) Add constants.ts file

--- components/Button/style.ts
+++ components/Button/style.ts
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import constants from "../../styles/constants";
 
 export const StyledButton = styled.button`
   border-radius: 90px;
   border: transparent;
   height: 60px;
-  width: ${props=>props.size || '347px'};
+  width: ${(props) => props.size || constants.defaultButtonWidth};
   color: white;
   font-family: "Poppins";
   font-size: 1rem;
@@ -12,9 +13,9 @@ export const StyledButton = styled.button`
 `;
 
 export const StyledGreenButton = styled(StyledButton)`
-  background: linear-gradient(91.71deg, #67BC45 7.99%, #096A09 94.71%);
+  background: linear-gradient(91.71deg, #67bc45 7.99%, #096a09 94.71%);
 `;
 
 export const StyledRedButton = styled(StyledButton)`
-  background: linear-gradient(91.71deg, #F23232 7.99%, #890909 94.71%);
+  background: linear-gradient(91.71deg, #f23232 7.99%, #890909 94.71%);
 `;
