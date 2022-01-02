9239f8243ebad63d4296481bdfae34c63e292d31
Gauthier Pailhas <ArgillaSeyshiro>
2022-01-02 19:54:11 +0100

    :sparkles: (Admin dashboard) Create the architecture of admin dashboard

---
+++ pages/settings/files.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Head from "next/head";
+import { DefaultBackground } from "../../components/Background/style";
+import {GreenButton} from "../../components/Button/Buttons";
+import { 
+  Row, 
+  Col6, 
+  Column
+} from "../../components/Container/style";
+import { 
+  HelpSection, 
+  TitleCard, 
+  ContributionStatus,
+  AdminMenu
+} from "../../components/Card/Cards"
+import { 
+  BlackText, 
+  BlackTitle, 
+  StyledCard 
+} from "../../components/Card/style";
+
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title>Mon Espace &bull; AMNet</title>
+      </Head>
+      <DefaultBackground style={{padding: "1% 2%"}}>
+        <div style={{width:"85px"}}>
+          <AdminMenu page="home" />
+        </div>
+
+      </DefaultBackground>
+    </>
+  );
+}
