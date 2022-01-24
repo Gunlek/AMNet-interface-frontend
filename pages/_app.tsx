e761c9a964b79b1ff11bff3458ec589e239be0be
ArgillaSeyshiro <gauthier.pailhas@gmail.com>
2022-01-24 10:46:15 +0100

    :sparkles: (Screen) Block zooming on phone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
           <Head>
             <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
             <link rel="alternate icon" href="/favicon.ico"/>
+            <meta name="viewport" content="width=device-width, user-scalable=no"/>
           </Head>
           <Component {...pageProps} />
         </>
