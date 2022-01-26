623aeb93c90b5d911d4a6911cd42189c19232ff7
ArgillaSeyshiro <gauthier.pailhas@gmail.com>
2022-01-26 10:26:39 +0100

    :sparkles: (Admin Table) Add table created from list of user requests

--- pages/settings/iot.tsx
+++ pages/settings/iot.tsx
@@ -4,26 +4,57 @@ import { DefaultBackground } from "../../components/Background/style";
 import { StyledCard } from "../../components/Card/style";
 import AdminMenu from "../../components/Menu/AdminMenu";
 import { DashboardContainer, Row } from "../../components/Container/style";
-import { BlackTitle, StyledLink } from "../../components/Text/style";
-import { 
-  StyledGreenTr, 
-  StyledTable, 
-  StyledTd150, 
-  StyledTd200, 
-  StyledTd50, 
-  StyledTd500, 
-  StyledTr 
-} from "../../components/Table/style";
-import { SmallGreenButton, SmallRedButton } from "../../components/Button/Buttons";
-import { StateRequest } from "../../components/Status/Status";
+import { BlackTitle } from "../../components/Text/style";
 import useMediaQuery from "../../components/MediaQueries/MediaQuery";
 import RequestTab from "../../components/Card/RequestTab";
+import { IoTAdminTable } from "../../components/Table/AdminTable";
 
-
+const Iot = [
+  {
+      "acces_id": 1,
+      "access_description": "Chromecast",
+      "access_mac": "AABBCCDDEEFF",
+      "access_proof": "photoProof-1621263399914.jpg",
+      "acces_user": 1,
+      "acces_state": "pending", 
+      "user_pay_status": 1,
+      "user_name": "Harwins",
+  },
+  {
+      "acces_id": 2,
+      "access_description": "Chromecast",
+      "access_mac": "AABBCCDDEEFF",
+      "access_proof": "photoProof-1621263399914.jpg",
+      "acces_user": 5,
+      "acces_state": "active",
+      "user_pay_status": 0,
+      "user_name": "Argilla",
+  },
+  {
+      "acces_id": 3,
+      "access_description": "Chromecast",
+      "access_mac": "AABBCCDDEEFF",
+      "access_proof": "photoProof-1621263399914.jpg",
+      "acces_user": 6,
+      "acces_state": "declined",
+      "user_pay_status": 1,
+      "user_name": "Greg"
+  },
+  {
+    "acces_id": 1,
+    "access_description": "Chromecast",
+    "access_mac": "AABBCCDDEEFF",
+    "access_proof": "photoProof-1621263399914.jpg",
+    "acces_user": 1,
+    "acces_state": "pending", 
+    "user_pay_status": 1,
+    "user_name": "Greg",
+  }
+]
 
 export default function Dashboard() {
   const minWidth1000 = useMediaQuery('(min-width:1000px)');
-  const [Tab, setTab] = useState("inProcess");
+  const [Tab, setTab] = useState("pending");
 
   const handleTabChange = (elmt) => {
     setTab(elmt.currentTarget.id);
@@ -46,42 +77,7 @@ export default function Dashboard() {
 
           <StyledCard style={{ flex: "1", marginBottom:"2%" }}>
             <div style={{ height:"100%", width:"100%", overflowX:"auto" }}>
-              <StyledTable>
-                <tbody>
-                  <StyledGreenTr style={{padding:"10px 0 10px 30px"}}>
-                    <StyledTd50>#</StyledTd50>
-                    <StyledTd150>Utilisateur</StyledTd150>
-                    <StyledTd200>Description</StyledTd200>
-                    <StyledTd200>Adresse Mac</StyledTd200>
-                    <StyledTd150>Preuve Image</StyledTd150>
-                    <StyledTd150 style={{marginRight:"20px"}}><span style={{paddingLeft: "5px"}}>Etat</span></StyledTd150>
-                    <StyledTd500><span style={{paddingLeft: "5px"}}>Action</span></StyledTd500>
-                  </StyledGreenTr>
-                  <StyledTr style={{padding:"10px 0 10px 30px"}}>
-                    <StyledTd50>1</StyledTd50>
-                    <StyledTd150>
-                      <StyledLink style={{fontSize:"1em"}} hovercolor="#67bc45">Gauthier</StyledLink>
-                    </StyledTd150>
-                    <StyledTd200>Test1</StyledTd200>
-                    <StyledTd200>aa:bb:cc:dd:ee:fff</StyledTd200>
-                    <StyledTd150><StyledLink style={{fontSize:"1em"}} hovercolor="#67bc45">Preuve Image</StyledLink></StyledTd150>
-                    <StyledTd150 style={{marginRight:"20px"}}><StateRequest state={"inProcess"}/></StyledTd150>
-                    <StyledTd500>
-                      <form 
-                        style={{ 
-                          width:"100%", 
-                          display:"flex", 
-                          justifyContent: "space-between"
-                        }}
-                      >
-                        <SmallGreenButton>Accorder</SmallGreenButton>
-                        <SmallRedButton>Revoquer</SmallRedButton>
-                        <SmallRedButton>Supprimer</SmallRedButton>
-                      </form>
-                    </StyledTd500>
-                  </StyledTr>
-                </tbody>
-              </StyledTable>
+              <IoTAdminTable status={Tab} RequestTable={Iot} />
             </div>
           </StyledCard>
         </DashboardContainer>
--- pages/settings/material.tsx
+++ pages/settings/material.tsx
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
 import Head from "next/head";
 import { DefaultBackground } from "../../components/Background/style";
-import { SmallGreenButton, SmallRedButton} from "../../components/Button/Buttons";
 import { Row, DashboardContainer } from "../../components/Container/style";
 import { StyledCard } from "../../components/Card/style";
 import AdminMenu from "../../components/Menu/AdminMenu";
-import { StateRequest } from "../../components/Status/Status";
-import { 
-  StyledTable, 
-  StyledGreenTr, 
-  StyledTd50, 
-  StyledTd150, 
-  StyledTd200, 
-  StyledTd500, 
-  StyledTr
-} from "../../components/Table/style";
-import { BlackTitle, StyledLink } from "../../components/Text/style";
+import { BlackTitle } from "../../components/Text/style";
 import useMediaQuery from "../../components/MediaQueries/MediaQuery";
 import RequestTab from "../../components/Card/RequestTab";
+import { MaterialAdminTable } from "../../components/Table/AdminTable";
+
+const Material = [
+  {
+      "material_id": 1,
+      "material_user": 1,
+      "material_description": "Ordinateur portable",
+      "material_state": "declined",
+      "user_pay_status": 1,
+      "user_name": "Greg"
+  },
+  {
+      "material_id": 2,
+      "material_user": 4,
+      "material_description": "Ordinateur portable",
+      "material_state": "pending",
+      "user_pay_status": 1,
+      "user_name": "Greg"
+  },
+  {
+      "material_id": 1,
+      "material_user": 5,
+      "material_description": "Ordinateur portable",
+      "material_state": "active",
+      "user_pay_status": 1,
+      "user_name": "Greg"
+  }
+]
 
 export default function Dashboard() {
   const minWidth1000 = useMediaQuery('(min-width:1000px)');
-  const [Tab, setTab] = useState("inProcess");
+  const [Tab, setTab] = useState("pending");
 
   const handleTabChange = (elmt) => {
     setTab(elmt.currentTarget.id);
@@ -44,36 +61,7 @@ export default function Dashboard() {
 
           <StyledCard style={{ flex: "1", marginBottom: minWidth1000 ? "2%" : "4%" }}>
             <div style={{ height:"100%", width:"100%", overflowX:"auto" }}>
-              <StyledTable>
-                <tbody>
-                  <StyledGreenTr style={{padding:"10px 0 10px 30px"}}>
-                    <StyledTd50>#</StyledTd50>
-                    <StyledTd150>Utilisateur</StyledTd150>
-                    <StyledTd200>Description</StyledTd200>
-                    <StyledTd150 style={{marginRight:"20px"}}><span style={{paddingLeft: "5px"}}>Etat</span></StyledTd150>
-                    <StyledTd500><span style={{paddingLeft: "5px"}}>Action</span></StyledTd500>
-                  </StyledGreenTr>
-                  <StyledTr style={{padding:"10px 0 10px 30px"}}>
-                    <StyledTd50>1</StyledTd50>
-                    <StyledTd150><StyledLink hovercolor="#67bc45" style={{fontSize:"1em"}}>Gauthier</StyledLink></StyledTd150>
-                    <StyledTd200>Test1</StyledTd200>
-                    <StyledTd150 style={{marginRight:"20px"}}><StateRequest state={"inProcess"}/></StyledTd150>
-                    <StyledTd500>
-                      <form 
-                        style={{ 
-                          width:"100%", 
-                          display:"flex", 
-                          justifyContent: "space-between"
-                        }}
-                      >
-                        <SmallGreenButton>Accorder</SmallGreenButton>
-                        <SmallRedButton>Revoquer</SmallRedButton>
-                        <SmallRedButton>Supprimer</SmallRedButton>
-                      </form>
-                      </StyledTd500>
-                  </StyledTr>
-                </tbody>
-              </StyledTable>
+              <MaterialAdminTable status={Tab} RequestTable={Material}/>
             </div>
           </StyledCard>
         </DashboardContainer>
