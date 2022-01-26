74f70fcaabc610ab17c9b2afa068c4390fec4920
ArgillaSeyshiro <gauthier.pailhas@gmail.com>
2022-01-26 10:31:26 +0100

    :bug: (Table) Correction of width column

--- components/Table/style.ts
+++ components/Table/style.ts
@@ -3,70 +3,52 @@ import styled from "styled-components";
 export const StyledTable = styled.table`
   width: 100%;
   font-family: "Poppins";
+  font-size: 1.2rem; 
+  border-collapse: collapse;
+  text-decoration: none;
+  table-layout: auto;
 `;
 
 export const StyledTr = styled.tr`
-  padding: 10px 30px;
-  display: flex;
   border-bottom: 2px solid rgba(0, 0, 0, 0.1);
   color: black;
-  font-size: 1.2em; 
 `;
 
 export const StyledGreenTr = styled.tr`
-  padding: 10px 30px;
-  display: flex;
-  border-radius: 14px;
   background: #096A09;
   color: white;
-  font-size: 1.2em;
-
 `;
 
 export const StyledTd = styled.td`
-  display: flex;
-  align-items: center;
+  padding : 15px 25px;
+  white-space: nowrap;
+  
+  &:first-child {
+    padding-left: 20px;
+  }
+  
+  &:last-child {
+    padding-right: 0;
+  }
 `;
 
-export const StyledTd50 = styled(StyledTd)`
-  flex: 1;
-  min-width: 50px;
+export const StyledFlexTd = styled(StyledTd)`
+  width:100vw;
 `;
 
-export const StyledTd100 = styled(StyledTd)`
-  flex: 2;
-  min-width: 100px;
-`;
+export const StyledTh = styled.th`
+  font-weight: normal;
+  text-align: start;
+  padding : 15px 25px;
 
-export const StyledTd150 = styled(StyledTd)`
-  flex: 3;
-  min-width: 150px;
-`;
+  &:first-child {
+    padding-left: 20px;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+  }
 
-export const StyledTd200 = styled(StyledTd)`
-  flex: 4;
-  min-width: 200px;
-`;
-
-export const StyledTd250 = styled(StyledTd)`
-  flex: 5;
-  min-width: 250px;
-`;
-
-export const StyledTd350 = styled(StyledTd)`
-  flex: 7;
-  min-width: 350px;
-`;
-
-export const StyledTd400 = styled(StyledTd)`
-  flex: 7;
-  min-width: 400px;
-`;
-
-export const StyledTd500 = styled(StyledTd)`
-  width: 500px;
-`;
-
-export const StyledTd850 = styled(StyledTd)`
-  width: 850px;
+  &:last-child {
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
 `;
