ce085c743f84ca8d5035d66ac5d454551fdec09d
ArgillaSeyshiro <gauthier.pailhas@gmail.com>
2022-01-31 19:45:26 +0100

    :pencil2: (Paragraph) Replace div by paragraph

--- pages/dashboard/material.tsx
+++ pages/dashboard/material.tsx
@@ -10,7 +10,7 @@ import {
 import { Footer, HelpSection } from "../../components/Card/Cards"
 import { StyledCard } from "../../components/Card/style";
 import UserMenu from "../../components/Menu/UserMenu";
-import { BlackTitle, BlackText } from "../../components/Text/style";
+import { BlackTitle, BlackText, BlackP } from "../../components/Text/style";
 import useMediaQuery from "../../components/MediaQueries/MediaQuery";
 import { MaterialUserTable } from "../../components/Table/User";
 import { ModalLogic, MaterialModal } from "../../components/Card/Modals";
@@ -68,9 +68,10 @@ export default function UserMaterial() {
           </ResponsiveRow>
 
           <Column style={{ marginBottom: "2%" }}>
-            <BlackText>
-              <span style={{ color: "#096a09", fontWeight: "bold" }}>Cette page</span> vous permettra de faire une demande de matériel comme un cable ethernet, un écran ou tout autre materiel à notre disposition
-            </BlackText>
+            <BlackP>
+               Pour compléter votre installation, vous pouvez avoir besoin d'un écran, d'un cable ethernet ou de matériel informatique.
+               <br/><span style={{ color: "#096a09", fontWeight: "bold" }}>Cette page</span> page est là pour vous permettre de formuler vos besoins dans la limite de nos stocks. Nous reviendrons vers vous dès que votre demande aura été étudiée !
+            </BlackP>
           </Column>
 
           <StyledCard style={{ flex: "1", marginBottom: minWidth1000 ? "2%" : "4%" }}>
