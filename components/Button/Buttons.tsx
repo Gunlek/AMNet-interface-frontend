f2940eac734a6a5228b135f1d4167bdb404c7742
ArgillaSeyshiro <gauthier.pailhas@gmail.com>
2022-02-10 19:01:33 +0100

    :sparkles: (Button) Add type to props

--- components/Button/Buttons.tsx
+++ components/Button/Buttons.tsx
@@ -8,35 +8,81 @@ import {
   SmallStyledOrangeButton
 } from "./style";
 
-type ButtonProps = { children: React.ReactNode; width?: string, height?: string, onClick?: Function };
+type ButtonProps = { children: React.ReactNode; width?: string, height?: string, onClick?: Function, type?: string };
 
 export function GreenButton(props: ButtonProps) {
-  return (
-    <StyledGreenButton width={props.width} height={props.height} onClick={props.onClick}>{props.children}</StyledGreenButton>
+  return(
+    <StyledGreenButton 
+      type={props.type} 
+      width={props.width} 
+      height={props.height} 
+      onClick={props.onClick}
+    >
+      {props.children}
+    </StyledGreenButton>
   );
 }
 
 export function SmallGreenButton(props: ButtonProps) {
-  return (
-    <SmallStyledGreenButton width={props.width} height={props.height} onClick={props.onClick}>{props.children}</SmallStyledGreenButton>
+  return(
+    <SmallStyledGreenButton 
+      type={props.type} 
+      width={props.width} 
+      height={props.height} 
+      onClick={props.onClick}
+    >
+      {props.children}
+    </SmallStyledGreenButton>
   );
 }
 
 export function RedButton(props: ButtonProps) {
-  return <StyledRedButton width={props.width} height={props.height} onClick={props.onClick}>{props.children}</StyledRedButton>;
+  return( 
+    <StyledRedButton 
+      type={props.type} 
+      width={props.width} 
+      height={props.height} 
+      onClick={props.onClick}
+    >
+      {props.children}
+    </StyledRedButton>);
 }
 
 export function SmallRedButton(props: ButtonProps) {
-  return (
-    <SmallStyledRedButton width={props.width} height={props.height} onClick={props.onClick}>{props.children}</SmallStyledRedButton>
+  return(
+    <SmallStyledRedButton 
+      type={props.type} 
+      width={props.width} 
+      height={props.height} 
+      onClick={props.onClick}
+    >
+      {props.children}
+    </SmallStyledRedButton>
   );
 }
 
 
 export function OrangeButton(props: ButtonProps) {
-  return <StyledOrangeButton width={props.width} height={props.height} onClick={props.onClick}>{props.children}</StyledOrangeButton>;
+  return(
+    <StyledOrangeButton 
+      type={props.type} 
+      width={props.width} 
+      height={props.height} 
+      onClick={props.onClick}
+    >
+      {props.children}
+    </StyledOrangeButton>);
 }
 
 export function SmallOrangeButton(props: ButtonProps) {
-  return <SmallStyledOrangeButton width={props.width} height={props.height} onClick={props.onClick}>{props.children}</SmallStyledOrangeButton>;
+  return(
+    <SmallStyledOrangeButton 
+      type={props.type} 
+      width={props.width} 
+      height={props.height} 
+      onClick={props.onClick}
+    >
+      {props.children}
+    </SmallStyledOrangeButton>
+  );
 }
