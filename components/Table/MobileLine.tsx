3aaf5b203bb87b0d6c009d0eeb535d4aa606b7e8
ArgillaSeyshiro <gauthier.pailhas@gmail.com>
2022-02-20 20:12:32 +0100

    :sparkles: (Mobil Line) Finish the drawer system on phone

--- components/Table/MobileLine.tsx
+++ components/Table/MobileLine.tsx
@@ -1,10 +1,10 @@
-import {StyledHeadTr, StyledTd, StyledTh, StyledTr} from "./style";
-import {StyledLink} from "../Text/style";
-import {StateRequest} from "../Status/Status";
-import React, {useState} from "react";
-import {Buttons} from "./Admin";
+import { StyledHeadTr, StyledTable, StyledTd, StyledTh, StyledTr } from "./style";
+import { StateRequest } from "../Status/Status";
+import React, { useState } from "react";
+import { Buttons } from "./Admin";
+import { ProoveModal } from "../Card/Modals";
 
-export const MobileLine = ({index, value, status}: {
+export const MobileLine = ({ index, value, status, isLast }: {
     index: number,
     value: {
         access_id: string,
@@ -16,57 +16,89 @@ export const MobileLine = ({index, value, status}: {
         user_pay_status: number,
         user_name: string
     },
-    status: string
+    status: string,
+    isLast: boolean
 }) => {
 
     const [scrolled, setScrolled] = useState(false);
 
     return (
-        <React.Fragment key={index}>
+        <>
             <StyledHeadTr onClick={() => setScrolled(!scrolled)}>
-                <StyledTh>Equipement {index}</StyledTh>
-                <StyledTh style={{textAlign: "center"}}>{value['access_description']}</StyledTh>
+                <StyledTh style={{ width: "130px" }}>Equipement {index}</StyledTh>
+                <StyledTh style={{ textAlign: "center" }}>{value['access_description']}</StyledTh>
             </StyledHeadTr>
-            <StyledTr style={{ display: scrolled ? "table-row" : "none"}}>
-                <StyledTd>Utilisateur</StyledTd>
-                <StyledTd style={{textAlign: "center"}}>{value['user_name']}</StyledTd>
-            </StyledTr>
-            <StyledTr style={{ display: scrolled ? "table-row" : "none"}}>
-                <StyledTd>Cotisation</StyledTd>
-                <StyledTd style={{textAlign: "center"}}>
-                <img style={{ height: "20px" }} src={value['user_pay_status'] ? "/static/icons/succes.svg" : "/static/icons/fail.svg"} />
-                </StyledTd>
-            </StyledTr>
-            <StyledTr style={{ display: scrolled ? "table-row" : "none"}}>
-                <StyledTd>Adresse Mac</StyledTd>
-                <StyledTd style={{textAlign: "center"}}>{value['access_mac']}</StyledTd>
-            </StyledTr>
-            <StyledTr style={{ display: scrolled ? "table-row" : "none"}}>
-                <StyledTd>Preuve</StyledTd>
-                <StyledTd style={{textAlign: "center"}}>
-                <StyledLink color="#096a09" hovercolor="#67bc45">Image</StyledLink>
-                </StyledTd>
-            </StyledTr>
-            <StyledTr style={{ display: scrolled ? "table-row" : "none"}}>
-                <StyledTd>Etat</StyledTd>
-                <StyledTd><StateRequest center={true} state={value['acces_state']} /></StyledTd>
-            </StyledTr>
-            <StyledTr style={{ height: "0px", overflow: "hidden", display: scrolled ? "table-row" : "none"}}>
-                <StyledTd>Actions</StyledTd>
-                <StyledTd style={{textAlign: "center"}}>
+            <tr>
+                <td colSpan={2} style={{ padding: "0" }}>
                     <div
                         style={{
-                        height: (status == "pending") ? "160px" : "100px",
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent:"space-between",
-                            flexDirection: "column"
-                    }}
-                >
-                    <Buttons status={value['acces_state']} />
+                            height: scrolled ? "485px" : "0px",
+                            transition: "0.3s linear",
+                            overflowY: "hidden",
+                            overflowX: "auto"
+                        }}
+                    >
+                        <StyledTable style={{ tableLayout: "fixed" }}>
+                            <tbody>
+                                <StyledTr>
+                                    <StyledTd style={{ width: "130px" }}>Utilisateur</StyledTd>
+                                    <StyledTd style={{ textAlign: "center" }}>{value['user_name']}</StyledTd>
+                                </StyledTr>
+                                <StyledTr>
+                                    <StyledTd>Cotisation</StyledTd>
+                                    <StyledTd style={{ textAlign: "center" }}>
+                                        <img 
+                                            style={{ height: "20px" }} 
+                                            src={value['user_pay_status'] ? "/static/icons/succes.svg" : "/static/icons/fail.svg"} 
+                                        />
+                                    </StyledTd>
+                                </StyledTr>
+                                <StyledTr>
+                                    <StyledTd>Adresse Mac</StyledTd>
+                                    <StyledTd style={{ textAlign: "center" }}>{value['access_mac']}</StyledTd>
+                                </StyledTr>
+                                <StyledTr>
+                                    <StyledTd>Preuve</StyledTd>
+                                    <StyledTd style={{ textAlign: "center", whiteSpace: "normal" }}>
+                                        <ProoveModal request={value} link="/static/images/campus.png" />
+                                    </StyledTd>
+                                </StyledTr>
+                                <StyledTr>
+                                    <StyledTd>Etat</StyledTd>
+                                    <StyledTd><StateRequest center={true} state={value['acces_state']} /></StyledTd>
+                                </StyledTr>
+                                <StyledTr>
+                                    <StyledTd>Actions</StyledTd>
+                                    <StyledTd style={{ textAlign: "center" }}>
+                                        <div
+                                            style={{
+                                                height: (status == "pending") ? "160px" : "100px",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "space-between",
+                                                flexDirection: "column"
+                                            }}
+                                        >
+                                            <Buttons status={value['acces_state']} />
+                                        </div>
+                                    </StyledTd>
+                                </StyledTr>
+                            </tbody>
+                        </StyledTable>
                     </div>
-                </StyledTd>
-            </StyledTr>
-        </React.Fragment>
+                </td>
+            </tr>
+            <tr style={{ display: isLast ? "none" : undefined }}>
+                <td style={{ padding: "0" }}>
+                    <div 
+                        style={{ 
+                            height: scrolled ? "0" : "30px", 
+                            transition: "0.3s linear", 
+                            overflow: "hidden" 
+                        }}
+                    />
+                </td>
+            </tr>
+        </>
     );
 }
