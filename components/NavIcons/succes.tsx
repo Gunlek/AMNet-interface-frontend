9953b01e6aa02fbdcbbb63c80c0f2d3243e4ff31
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-03-22 12:38:22 +0100

    :sparkles: (Icon SVG) Create React Component to load faster

---
+++ components/NavIcons/succes.tsx
@@ -0,0 +1,7 @@
+
+export const Succes = (props: {marginRight?: string}) => (
+    <svg style={{ marginRight: props.marginRight }} width="22" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21.451.55a1.87 1.87 0 0 1 0 2.646l-12.17 12.17a1.867 1.867 0 0 1-2.647 0L.549 9.281a1.87 1.87 0 0 1 0-2.647 1.87 1.87 0 0 1 2.647 0l4.762 4.761L18.804.55a1.87 1.87 0 0 1 2.647 0Z" fill="#67BC45"/>
+    </svg>
+);
+
+export default Succes
