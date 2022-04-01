70af64d99ea8d211ce41c5a1aa52952b52bd4a58
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-04-01 10:49:25 +0200

    :recycle: (NavIcons) Delete useless component

--- components/NavIcons/style.ts
+++ components/NavIcons/style.ts
@@ -171,13 +171,3 @@ export const StyledGitHub = styled.path`
     fill: #67bc45;
   }
 `;
-
-export const StyledBackArrow = styled.div`
-  height: 50px;
-  width: 50px;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transform: ${(props) => "translateY("+props.translate+")"} ;
-`
