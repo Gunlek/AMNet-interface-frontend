305915555190c0a73d138b2883cb76d6153bd9c1
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-04-07 11:56:58 +0200

    :lipstick: (404 Text) Align Text justify

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Head from "next/head";
 import { CampusGlobalStyle } from "../components/Background/style";
-import { GreenButton } from "../components/Button/Buttons";
+import { ButtonLink, GreenButton } from "../components/Button/Buttons";
 import { TitleCard, HelpSection, Footer } from "../components/Card/Cards";
 import RectangleLogo from "../components/Card/RectangleLogo";
 import { StyledCardCampus } from "../components/Card/style";
 import { Row } from "../components/Container/style";
-import { BlackText, StyledLink, StyledLinkButton } from "../components/Text/style";
+import { BlackText, StyledLink,  } from "../components/Text/style";
 
 export default function Page404() {
   return (
@@ -30,8 +30,8 @@ export default function Page404() {
           </Row>
 
           <TitleCard>Oup's... Erreur 404</TitleCard>
-          <BlackText>
-            Il semble que même le pouvoir de la 47-102 ne suffise pas à trouver la page que vous recherchez...<br/><br/>
+          <BlackText style={{ textAlign: "justify" }}>
+            Il semble que même le pouvoir de la 47-102 ne suffise pas à trouver la page que vous recherchez...<br /><br />
             La dernière solution serait sans doute d'invoquer le gorg'ss, pour cela il faut contacter la rochefouc's par le <StyledLink color="#096a09" href="mailto:contact@amnet.fr">channel 58</StyledLink>
           </BlackText>
 
@@ -43,9 +43,7 @@ export default function Page404() {
               marginTop: "20px"
             }}
           >
-            <StyledLinkButton href="./" style={{ borderRadius: "90px" }}>
-              <GreenButton>Accéder à l'acceuil</GreenButton>
-            </StyledLinkButton>
+            <ButtonLink href="/">Accéder à l'accueil</ButtonLink>
           </Row>
 
         </StyledCardCampus>
