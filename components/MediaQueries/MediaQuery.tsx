6965a7d9df1971721c148f9190f9bb2cad662e92
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-04-12 12:44:59 +0200

    :sparkles: (useMediaQuery) Add type for the props

--- components/MediaQueries/MediaQuery.tsx
+++ components/MediaQueries/MediaQuery.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default function useMediaQuery(query) {
+export default function useMediaQuery(query: string) {
     const [matches, setMatches] = useState(false);
 
     useEffect(() => {
