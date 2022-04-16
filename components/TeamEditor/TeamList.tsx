e9f52844d57ac5ec833db5509b40ba2f0b899406
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-04-16 15:14:13 +0200

    :bug: (Team List) Wrong indexUp on phone

--- components/TeamEditor/TeamList.tsx
+++ components/TeamEditor/TeamList.tsx
@@ -20,35 +20,24 @@ const Row = ({ row, index, moveRow, deleteTeamMember }) => {
       }
       const dragIndex = item.index
       const hoverIndex = index
-      // Don't replace items with themselves
+
       if (dragIndex === hoverIndex) {
         return
       }
-      // Determine rectangle on screen
+
       const hoverBoundingRect = dropRef.current.getBoundingClientRect()
-      // Get vertical middle
       const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
-      // Determine mouse position
       const clientOffset = monitor.getClientOffset()
-      // Get pixels to the top
       const hoverClientY = clientOffset.y - hoverBoundingRect.top
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-      // Dragging downwards
+
       if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
         return
       }
-      // Dragging upwards
       if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
         return
       }
-      // Time to actually perform the action
+      
       moveRow(dragIndex, hoverIndex)
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
       item.index = hoverIndex
     },
   })
@@ -114,7 +103,7 @@ const Row = ({ row, index, moveRow, deleteTeamMember }) => {
 
 const MobileRow = ({ row, index, moveRow, deleteTeamMember, lastIndex }) => {
   const newIndexDown = index == lastIndex ? 0 : index + 1;
-  const newIndexUp = index == 0 ? lastIndex : index;
+  const newIndexUp = index == 0 ? lastIndex : index-1;
 
   return (
     <StyledTr>
