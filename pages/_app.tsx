2de78d36cbfb82a14407432ababcc21318226459
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-04-19 20:30:05 +0200

    :zap: (Document) Add some props to head document instead of head next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,24 +9,14 @@ import Homepage from "./homepage";
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   const pathname = router.pathname.split('/')[1]
-  
+
   const isLogged = true;
   const isRedirected = !isLogged && (pathname !== "homepage")
 
   if (isRedirected) { typeof window !== 'undefined' && router.push('/homepage') }
-  
+
   return <>
     <Head>
-      <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-      <link rel="manifest" href="/favicons/site.webmanifest" />
-      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#79c45d" />
-      <link rel="prefetch" as="font" href="/static/Poppins-Medium.ttf" type="font/ttf" crossOrigin="anonymous" />
-      <meta name="msapplication-TileColor" content="#79c45d" />
-      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-      <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, user-scalable=no" />
     </Head>
     <GlobalStyle />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,13 +39,25 @@ export default class MyDocument extends Document {
             type="text/css"
             dangerouslySetInnerHTML={{ __html: mediaStyles }}
           />
+          <link rel="preload" as="font" href="/static/Poppins-Medium.ttf" type="font/ttf" crossOrigin="anonymous" />
+          <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#79c45d" />
+          <meta name="msapplication-TileColor" content="#79c45d" />
+          <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
+        
         <body>
+        
           <Main />
           <NextScript />
         </body>
       </Html>
     )
   }
-  
+
 }
