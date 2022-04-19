447b792aeb2fcace8b5d42a452ca8f6f4464c80f
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-04-19 18:55:43 +0200

    :recycle: (FAQ) Clear an old useless code

--- pages/homepage/faq.tsx
+++ pages/homepage/faq.tsx
@@ -1,83 +1,32 @@
 import React from "react";
 import Head from "next/head";
 import { CampusGlobalStyle } from "../../components/Background/style";
-import { Footer, HelpSection, TitleCard } from "../../components/Card/Cards";
-import { StyledCardCampus } from "../../components/Card/style";
-import { Col6, ResponsiveRow } from "../../components/Container/style";
+import { Footer, HelpSection } from "../../components/Card/Cards";
+import { Col6, Column, Row } from "../../components/Container/style";
 import RectangleLogo from "../../components/Card/RectangleLogo";
-import { GreenButton } from "../../components/Button/Buttons";
-import { BlackP } from "../../components/Text/style";
-import useMediaQuery from "../../components/MediaQueries/MediaQuery";
+import { ButtonLink } from "../../components/Button/Buttons";
+
 
 export default function FAQ() {
-  const minWidth1000 = useMediaQuery('(min-width:1000px)');
   return (
     <>
       <Head>
         <title>FAQ &bull; AMNet</title>
       </Head>
       <CampusGlobalStyle />
-        <ResponsiveRow style={{ marginBottom: "2%" }}>
-          <Col6 style={{ justifyContent: minWidth1000 ? "start" : "center", alignItems: minWidth1000 ? "start" : "center" }}>
+
+      <Column style={{ padding: "0 5%", flex: "1" }}>
+        <Row margin="20px 0" mobileMargin="30px 0" direction="column">
+          <Col6 mobileMarginBottom="30px" justify="center" mobileAlign="center">
             <RectangleLogo color="white" />
           </Col6>
-          <Col6 style={{ justifyContent: "center", alignItems: minWidth1000 ? "end" : "center", marginTop: minWidth1000 ? "0" : "20px" }}>
-            <a
-              href="./login"
-              style={{
-                width: minWidth1000 ? "300px" : "250px",
-                borderRadius: "30px"
-              }}
-            >
-              <GreenButton width="100%">Se Connecter / S'inscrire</GreenButton>
-            </a>
+          <Col6 align="end" mobileAlign="center" justify="center">
+            <ButtonLink href="/homepage/login" width="300px">Se Connecter / S'inscrire</ButtonLink>
           </Col6>
-        </ResponsiveRow>       
+        </Row>
+      </Column>
 
-        <StyledCardCampus style={{ marginBottom: "2%" }}>          
-          <TitleCard>Comment avoir Internet</TitleCard>
-          <BlackP>
-            Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebantur
-          </BlackP>
-        </StyledCardCampus>
-
-        <StyledCardCampus style={{ marginBottom: "2%" }}>          
-          <TitleCard>Se connecter au Wi-Fi</TitleCard>
-          <BlackP>
-            Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebantur
-          </BlackP>
-        </StyledCardCampus>
-
-        <StyledCardCampus style={{ marginBottom: "2%" }}>          
-          <TitleCard>Se connecter en câble</TitleCard>
-          <BlackP>
-            Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebantur
-          </BlackP>
-        </StyledCardCampus>
-
-        <StyledCardCampus style={{ marginBottom: "2%" }}>          
-          <TitleCard>Se connecter à AMNet IoT</TitleCard>
-          <BlackP>
-            Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebantur
-          </BlackP>
-        </StyledCardCampus>
-
-        <StyledCardCampus style={{ marginBottom: "2%" }}>          
-          <TitleCard>Qui gère le réseau</TitleCard>
-          <BlackP>
-            Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebantur
-            Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebantur
-          </BlackP>
-        </StyledCardCampus>
-
-        <StyledCardCampus style={{ marginBottom: "2%" }}>          
-          <TitleCard>Card en plus</TitleCard>
-          <BlackP>
-            Et interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebanturEt interdum acciderat, ut siquid in penetrali secreto nullo citerioris vitae ministro praesente paterfamilias uxori susurrasset in aurem, velut Amphiarao referente aut Marcio, quondam vatibus inclitis, postridie disceret imperator. ideoque etiam parietes arcanorum soli conscii timebantur
-          </BlackP>
-        </StyledCardCampus>
-
-        <HelpSection/>
+      <HelpSection />
       <Footer page="campus" />
     </>
   );
