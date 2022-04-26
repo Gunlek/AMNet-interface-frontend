ab97003b17505a8f750f582e0183116fc57226ab
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-04-26 16:30:04 +0200

    :art: (App) refactoring the code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import { GlobalStyle } from "../styles/global";
 import "../styles/globals.css";
 import Homepage from "./homepage";
 
-
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
   const pathname = router.pathname.split('/')[1]
@@ -13,15 +12,17 @@ function MyApp({ Component, pageProps }) {
   const isLogged = true;
   const isRedirected = !isLogged && (pathname !== "homepage")
 
-  if (isRedirected) { typeof window !== 'undefined' && router.push('/homepage') }
+  if (isRedirected) { typeof window !== 'undefined' && router.replace('/homepage') }
 
-  return <>
-    <Head>
-      <meta name="viewport" content="width=device-width, user-scalable=no" />
-    </Head>
-    <GlobalStyle />
-    {isRedirected ? <Homepage /> : <Component {...pageProps} />}
-  </>
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Head>
+      <GlobalStyle />
+      {isRedirected ? <Homepage /> : <Component {...pageProps} />}
+    </>
+  )
 }
 
 export default MyApp;
