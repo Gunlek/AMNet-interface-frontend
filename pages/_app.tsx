1802f0a1c5ae08fec15fa97893d1fd34717383f3
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-01 19:08:10 +0200

    :construction: (Conditional display if connected)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import 'regenerator-runtime/runtime'
 import { GlobalStyle } from "../styles/global";
 import "../styles/globals.css";
-import Homepage from "./homepage";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -20,7 +19,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, user-scalable=no" />
       </Head>
       <GlobalStyle />
-      {isRedirected ? <Homepage /> : <Component {...pageProps} />}
+       <Component {...pageProps} />
     </>
   )
 }
