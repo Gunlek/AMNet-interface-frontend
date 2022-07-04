8cba19fd55db89b3799aedce5b65340c9b9c57c1
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-04 23:03:38 +0200

    :fire: (Users) Delete the old users table remplaced by users2

--- components/Table/Admin/Users.tsx
+++ components/Table/Admin/Users.tsx
@@ -1,21 +1,24 @@
-import React, { forwardRef, useEffect, useMemo, useRef, useState } from 'react'
-import { useTable, useFilters, useGlobalFilter, useAsyncDebounce, useRowSelect, useSortBy } from 'react-table'
+import React, { forwardRef, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useTable, useFilters, useGlobalFilter, useAsyncDebounce, useRowSelect, useSortBy, useBlockLayout, useFlexLayout } from 'react-table'
 import { CheckboxRow, Row } from '../../Container/style';
 import Checkbox from '../../Input/Checkbox';
 import { StyledInput, StyledLabel } from '../../Input/style';
 import { BlackText, StyledLink, WhiteText } from '../../Text/style';
 import {
-  StyledTable,
   StyledTh,
   StyledTd,
   StyledUsersTr,
-  StyledHeadTr
+  StyledHeadTr,
+  StyledUsersBody,
+  StyledMobileContainerRow
 } from '../style';
 import UsersMobileLine from './MobileLine/Users';
 import { MediaContextProvider, Media } from '../../MediaQueries/MediaSSR';
 import Fail from '../../NavIcons/fail';
 import Succes from '../../NavIcons/succes';
 import Link from 'next/link';
+import { FixedSizeList } from 'react-window'
+import AutoSizer from "react-virtualized-auto-sizer"
 
 interface Props {
   indeterminate?: boolean;
@@ -52,10 +55,9 @@ const IndeterminateCheckbox = forwardRef<HTMLInputElement, Props>(
     }, [combinedRef, indeterminate]);
 
     return (
-      <StyledLabel width="25px">
+      <StyledLabel Display="inline-flex" width="25px">
         <Checkbox refs={combinedRef} {...rest} />
       </StyledLabel>
-
     );
   }
 );
@@ -82,29 +84,27 @@ function GlobalFilter({ globalFilter, setGlobalFilter }) {
 export function UsersTable(data: any[]) {
   const columns = useMemo(
     () => [
-      { Header: '#', accessor: (row, i) => i + 1 },
-      { Header: 'Utilisateur', accessor: 'user_name' },
-      { Header: 'Prénom', accessor: 'user_firstname' },
-      { Header: 'Nom', accessor: 'user_lastname' },
-      { Header: 'Mail', accessor: 'user_email' },
-      { Header: 'Cotisation', accessor: 'user_pay_status' },
-      { Header: 'Téléphone', accessor: 'user_phone' },
-      { Header: 'Bucque', accessor: 'user_bucque' },
-      { Header: 'Fam\'s', accessor: 'user_fams' },
-      { Header: 'Campus', accessor: 'user_campus' },
-      { Header: 'Promotion', accessor: 'user_pomotion' },
-      { Header: 'Rank', accessor: 'user_rank' },
-      { Header: 'Gadz', accessor: 'user_is_gadz' },
+      { Header: '#', accessor: (row, i) => i + 1, width: 90 },
+      { Header: 'Utilisateur', accessor: 'user_name', minWidth: 250 },
+      { Header: 'Prénom', accessor: 'user_firstname', minWidth: 200 },
+      { Header: 'Nom', accessor: 'user_lastname', minWidth: 250 },
+      { Header: 'Mail', accessor: 'user_email', minWidth: 350 },
+      { Header: 'Cotiz', accessor: 'user_pay_status', width: 100 },
+      { Header: 'Téléphone', accessor: 'user_phone', width: 200 },
+      { Header: 'Bucque', accessor: 'user_bucque', minWidth: 250 },
+      { Header: 'Fam\'s', accessor: 'user_fams', width: 150 },
+      { Header: 'TBK', accessor: 'user_campus', width: 150 },
+      { Header: 'Prom\'s', accessor: 'user_proms', width: 100 },
+      { Header: 'Rank', accessor: 'user_rank', width: 100 },
+      { Header: 'Gadz', accessor: 'user_is_gadz', width: 100 },
+      { Header: 'Notification', accessor: 'user_notification', width: 125 },
       { Header: 'Id', accessor: 'user_id' }
     ]
     ,
     []
   )
 
-  // Use the state and functions returned from useTable to build your UI
   const {
-    getTableProps,
-    getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
@@ -121,34 +121,28 @@ export function UsersTable(data: any[]) {
         'user_phone',
         'user_bucque',
         'user_fams',
-        'user_fams',
         'user_campus',
-        'user_pomotion',
+        'user_proms',
         'user_rank',
         'user_is_gadz',
-        'user_id'
+        'user_id',
+        'user_notification'
       ]
     }
   },
     useFilters,
     useGlobalFilter,
     useSortBy,
+    useFlexLayout,
     useRowSelect,
     hooks => {
       hooks.visibleColumns.push(columns => [
-        // Let's make a column for selection
         {
           id: 'selection',
-          minWidth: 35,
-          width: 35,
-          maxWidth: 35,
-          // The header can use the table's getToggleAllRowsSelectedProps method
-          // to render a checkbox
+          width: 90,
           Header: ({ getToggleAllRowsSelectedProps }) => (
             <IndeterminateCheckbox Color="white" {...getToggleAllRowsSelectedProps()} />
           ),
-          // The cell can use the individual row's getToggleRowSelectedProps method
-          // to the render a checkbox
           Cell: ({ row }) => (
             <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
           ),
@@ -158,10 +152,170 @@ export function UsersTable(data: any[]) {
     }
   )
 
-  const ContainerStyle = {
-    height: "100%",
-    width: "100%",
-    overflow: "auto",
+  const RenderRow = useCallback(
+    ({ index, style }) => {
+      const row = rows[index]
+      prepareRow(row)
+      const remainder = index % 2;
+
+      return (
+        <StyledUsersTr as="div"  {...row.getRowProps({ style })}>
+          {row.cells.map((cell) => {
+            const replaceBySvg = (cell.column['id'] == 'user_pay_status' || cell.column['id'] == 'user_is_gadz' || cell.column['id'] == 'user_notification')
+            return (
+              <StyledTd
+                textAlign={replaceBySvg ? "center" : undefined}
+                BackgroundColor={remainder ? "rgba(0, 0, 0, 0.075)" : undefined}
+                {...cell.getCellProps()}
+                as="div"
+              >
+                {replaceBySvg ?
+                  cell.value ? <Succes /> : <Fail />
+                  :
+                  cell.column['id'] == 'user_name' ?
+                    <Link href={{
+                      pathname: '/admin/users/[user_id]',
+                      query: { user_id: row.allCells[15].value }
+                    }}
+                      prefetch={false}
+                      passHref>
+                      <StyledLink color="#096a09">
+                        {cell.render('Cell')}
+                      </StyledLink>
+                    </Link>
+                    :
+                    cell.render('Cell')
+                }
+              </StyledTd>
+            )
+          })}
+        </StyledUsersTr>
+      )
+    },
+    [prepareRow, rows]
+  )
+
+  const RenderMobileRow = useCallback(
+    ({ index, style }) => {
+      const row = rows[index]
+      prepareRow(row)
+      return (
+        <StyledMobileContainerRow  {...row.getRowProps({ style })}>
+          <UsersMobileLine
+            key={index}
+            row={row}
+            columnsNumber={14 - state.hiddenColumns.length}
+            isLast={index == (rows.length - 1)}
+          />
+        </StyledMobileContainerRow>
+      )
+    },
+    [prepareRow, rows]
+  )
+
+  const idSelected = selectedFlatRows.map(d => d.original["user_id"])
+
+  const innerElementType = forwardRef(({ children, ...rest }, ref) => (
+    <>
+      <div style={{ display: "flex", width: "100%", position: "sticky", top: "0", zIndex: "2" }}>
+        <StyledHeadTr {...headerGroups[0].getHeaderGroupProps()} as="div">
+          {headerGroups[0].headers.map((column, index) => {
+            const replaceBySvg = (column['id'] == 'user_pay_status' || column['id'] == 'user_is_gadz')
+
+            return (
+              <StyledTh textAlign={replaceBySvg ? "center" : undefined} {...column.getHeaderProps(column.getSortByToggleProps())} as="div">
+                {column.render('Header')}
+                {(index == 0) ?
+                  <span
+                    style={{
+                      display: "inline-block",
+                      transform: "translateY(-15%)",
+                      marginLeft: "10px"
+                    }}
+                  >
+                    {Object.keys(selectedRowIds).length}
+                  </span>
+                  :
+                  column.isSorted ? column.isSortedDesc ? '▼' : '▲' : ''
+                }
+              </StyledTh>
+            )
+          })}
+        </StyledHeadTr>
+      </div>
+
+      <StyledUsersBody {...rest}>
+        {children}
+      </StyledUsersBody>
+    </>
+  ));
+
+  const innerMobileElementType = forwardRef(({ children, ...rest }, ref) => (
+    <>
+      <Row style={{
+        marginBottom: "20px",
+        justifyContent: "space-between",
+        position: "sticky",
+        zIndex: "3",
+        top: "0",
+        borderRadius: "10px",
+        background: "#096A09",
+        color: "white",
+        padding: "15px 20px"
+      }}
+      >
+        <div
+          style={{
+            width: "55px",
+            display: "flex",
+            justifyContent: "space-between",
+            fontSize: "1.2rem",
+          }}
+        >
+          {Object.keys(selectedRowIds).length}
+          {headerGroups[0].headers[0].render('Header')}
+        </div>
+        <WhiteText style={{ paddingRight: "10px" }}>Tout sélectionner</WhiteText>
+      </Row>
+
+      <StyledUsersBody {...rest}>
+        {children}
+      </StyledUsersBody>
+    </>
+  ));
+
+  function Table() {
+    return (
+      <AutoSizer>
+        {({ height, width }) => (
+          <MediaContextProvider>
+            <Media at="sm">
+              <FixedSizeList
+                height={height}
+                itemCount={rows.length}
+                itemSize={86}
+                width={width}
+                innerElementType={innerMobileElementType}
+              >
+                {RenderMobileRow}
+              </FixedSizeList>
+            </Media>
+
+            <Media greaterThan="sm">
+              <FixedSizeList
+                height={height}
+                itemCount={rows.length}
+                itemSize={61}
+                width={width}
+                innerElementType={innerElementType}
+              >
+                {RenderRow}
+              </FixedSizeList>
+            </Media>
+          </MediaContextProvider>
+        )}
+      </AutoSizer>
+    )
   }
 
   return [
@@ -180,150 +334,8 @@ export function UsersTable(data: any[]) {
         </StyledLabel>
       ))}
     </CheckboxRow>,
-    selectedFlatRows.map(
-      d => d.original["user_id"]
-    ),
-    <MediaContextProvider>
-      <Media at="sm" style={ContainerStyle}>
-        <>
-          <Row style={{
-            marginBottom: "20px",
-            justifyContent: "space-between",
-            position: "sticky",
-            zIndex: "2",
-            top: "0",
-            borderRadius: "10px",
-            background: "#096A09",
-            color: "white",
-            padding: "15px 20px"
-          }}
-          >
-            <label
-              style={{
-                width: "55px",
-                display: "flex",
-                justifyContent: "space-between",
-                fontSize: "1.2rem",
-              }}
-            >
-              {Object.keys(selectedRowIds).length}
-              {headerGroups[0].headers[0].render('Header')}
-            </label>
-            <WhiteText style={{ paddingRight: "10px" }}>Tout sélectionner</WhiteText>
-          </Row>
-
-          {rows.map((row, index) => {
-            prepareRow(row)
-
-            return (
-              <UsersMobileLine
-                key={index}
-                row={row}
-                columnsNumber={14 - state.hiddenColumns.length}
-                isLast={index == (rows.length - 1)}
-              />
-            )
-          })}
-        </>
-      </Media>
-
-      <Media style={ContainerStyle} greaterThan="sm">
-        <StyledTable {...getTableProps()}>
-          <thead style={{ position: "sticky", top: "0", zIndex: "2" }}>
-            <StyledHeadTr {...headerGroups[0].getHeaderGroupProps()}>
-              {headerGroups[0].headers.map((column, index) => {
-                if (column['id'] == 'user_pay_status' || column['id'] == 'user_is_gadz') {
-                  return (
-                    <StyledTh {...column.getHeaderProps(column.getSortByToggleProps())}>
-                      <div style={{ display: "flex", userSelect: "none", justifyContent: "center" }}>
-                        {column.render('Header')}
-                        <span
-                          style={{
-                            display: "inline-block",
-                            marginLeft: "10px",
-                            width: "20px"
-                          }}
-                        >
-                          {column.isSorted ? column.isSortedDesc ? '▼' : '▲' : ''}
-                        </span>
-                      </div>
-                    </StyledTh>
-                  )
-                }
-                else {
-                  return (
-                    <StyledTh {...column.getHeaderProps(column.getSortByToggleProps())}>
-                      <div style={{ display: "flex", userSelect: "none" }}>
-                        {column.render('Header')}
-                        <span
-                          style={{
-                            display: "inline-block",
-                            marginLeft: "10px",
-                            width: "20px"
-                          }}
-                        >
-                          {(index == 0) && Object.keys(selectedRowIds).length}
-                          {!(index == 0) && column.isSorted ? column.isSortedDesc ? '▼' : '▲' : ''}
-                        </span>
-                      </div>
-                    </StyledTh>
-                  )
-                }
-              })}
-            </StyledHeadTr>
-          </thead>
-
-          <tbody {...getTableBodyProps()} style={{ position: "relative" }}>
-            {rows.map((row, index) => {
-              prepareRow(row)
-              return (
-                <StyledUsersTr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    if (cell.column['id'] == 'user_pay_status' || cell.column['id'] == 'user_is_gadz') {
-                      return (
-                        <StyledTd style={{ textAlign: "center" }}{...cell.getCellProps()}>
-                          {cell.value ? <Succes marginRight="25px" /> : <Fail marginRight="25px" />}
-                        </StyledTd>
-                      )
-                    }
-                    else if (cell.column['id'] == 'user_name') {
-                      return (
-                        <StyledTd {...cell.getCellProps()}>
-                          <Link href={{
-                            pathname: '/admin/users/[user_id]',
-                            query: { user_id: row.allCells[14].value },
-                          }}
-                            prefetch={false}
-                            passHref>
-                            <StyledLink color="#096a09">
-                              {cell.render('Cell')}
-                            </StyledLink>
-                          </Link>
-                        </StyledTd>
-                      )
-                    }
-                    else {
-                      return (
-                        <StyledTd {...cell.getCellProps()}>
-                          {cell.render('Cell')}
-                        </StyledTd>
-                      )
-                    }
-                  })}
-                </StyledUsersTr>
-              )
-            })}
-          </tbody>
-        </StyledTable>
-      </Media>
-    </MediaContextProvider>
+    idSelected
+    ,
+    Table
   ]
 }
-
-
-
-
-
-
-
-
