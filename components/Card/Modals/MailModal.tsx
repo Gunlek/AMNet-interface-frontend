3b2d400ebfeeb96a7b34e44229a6a32c948f11a0
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-09 20:43:13 +0200

    :recycle: (Mail Modal) Use new techno to display the mail template

--- components/Card/Modals/MailModal.tsx
+++ components/Card/Modals/MailModal.tsx
@@ -3,6 +3,10 @@ import useMediaQuery from "../../MediaQueries/MediaQuery"
 import { TitleCard } from "../Cards"
 import ModalLogic from "./ModalLogic"
 import { StyledBackgroundModal, StyledModal } from "./style"
+import Image from 'next/image'
+import RectangleLogo from "../RectangleLogo"
+import { StyledLink } from "../../Text/style"
+import { Row } from "../../Container/style"
 
 export default function MailModal(props: { html: any }) {
     const minWidth1000 = useMediaQuery('(min-width: 1000px)')
@@ -20,105 +24,89 @@ export default function MailModal(props: { html: any }) {
                         style={{ maxHeight: "90vh" }}
                     >
                         <TitleCard>Pré-visualisation du Mail</TitleCard>
-                        <div style={{ width: "100%", borderRadius: "20px", marginBottom: "30px", overflowY: "auto" }}>
+                        <div
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                overflow: "auto",
+                                borderRadius: "20px",
+                                marginBottom: "30px",
+                                background: "#E8EFEA ",
+                                paddingBottom: "20px"
+                            }}
+                        >
                             {Display &&
-                                <table style={{ background: "#E8EFEA", border: "none", borderCollapse: "collapse" }} width="100%">
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <table style={{ background: "#E8EFEA", border: "none", borderCollapse: "collapse", width: "600px", margin: "0 auto" }} width="600px">
-                                                    <tbody>
-                                                        <tr>
-                                                            <td style={{ textAlign: "center", background: "white", padding: "0" }}>
-                                                                <img style={{ width: "600px", display: "block", border: "none" }}
-                                                                    src="/static/images/template/info.png"
-                                                                />
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td style={{ padding: "0" }}>
-                                                                <table
-                                                                    style={{
-                                                                        background: "url(/static/images/template/body.png)",
-                                                                        backgroundSize: "contain",
-                                                                        border: "none",
-                                                                        borderCollapse: "collapse",
-                                                                        width: "600px",
-                                                                        backgroundColor: "#E8EFEA",
-                                                                        margin: "0 auto"
-                                                                    }}
-                                                                >
-                                                                    <tbody>
-                                                                        <tr>
-                                                                            <td>
-                                                                                <table style={{ width: "533px", margin: "0 auto" }}>
-                                                                                    <tbody>
-                                                                                        <tr>
-                                                                                            <td>
-                                                                                                <div dangerouslySetInnerHTML={{ __html: props.html || "" }} style={{ width: "100%", padding: "0 30px", fontSize: "18px" }}>
+                                <div style={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}>
+                                    <Image
+                                        width={600}
+                                        height={77}
+                                        src="/static/images/template/info.png"
+                                        alt="template info"
+                                        layout="fixed"
+                                    />
 
-                                                                                                </div>
-                                                                                            </td>
-                                                                                        </tr>
-                                                                                        <tr>
-                                                                                            <td style={{ textAlign: "center", fontSize: "18px", padding: "0 10px", color: "#096A09" }}>
-                                                                                                Besoin d'assistance ? <a
-                                                                                                    style={{ textDecoration: "none", color: "#096A09" }}
-                                                                                                    href="mailto:contact@amnet.fr">contact@amnet.fr</a>
-                                                                                            </td>
-                                                                                        </tr>
-                                                                                    </tbody>
-                                                                                </table>
-                                                                            </td>
-                                                                        </tr>
-                                                                    </tbody>
-                                                                </table>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td style={{ textAlign: "center", background: "white", padding: "0" }}>
-                                                                <img style={{ width: "600px", display: "block", border: "none" }}
-                                                                    src="/static/images/template/bottom.png"
-                                                                />
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <table style={{ border: "none", borderCollapse: "collapse", width: "600px", margin: "0 auto" }}>
-                                                    <tbody>
-                                                        <tr style={{ textAlign: "center" }}>
-                                                            <td>
-                                                                <a target="_blank" href="https://amnet.fr">
-                                                                    <img style={{ display: "inline-block", border: "none" }}
-                                                                        src="/static/images/template/amnet.png" />
-                                                                </a>
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td style={{ fontSize: "10px" }}>&nbsp;</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td style={{ textAlign: "center", fontSize: "12px", padding: "0 10px" }} >
-                                                                Pour vous désabonner <a href="/homepage/unsubscribe" target="_blank" style={{ textDecoration: "none", color: "#096A09" }}
-                                                                >Cliquez ici</a>
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style={{ fontSize: "10px" }}>&nbsp;</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                    <div
+                                        style={{
+                                            background: "url(/static/images/template/body.png)",
+                                            backgroundSize: "contain",
+                                            width: "600px",
+                                            padding: "0 40px",
+                                        }}
+                                    >
+                                        <div
+                                            dangerouslySetInnerHTML={{ __html: props.html || "" }}
+                                            style={{
+                                                width: "100%",
+                                                padding: "0 30px",
+                                                fontSize: "18px"
+                                            }}
+                                        />
+
+                                        <div style={{ textAlign: "center" }}>
+                                            Besoin d&apos;assistance ?{" "}
+                                            <StyledLink
+                                                color="#096A09"
+                                                href="mailto:contact@amnet.fr"
+                                            >
+                                                contact@amnet.fr
+                                            </StyledLink>
+                                        </div>
+                                    </div>
+
+                                    <Image
+                                        width={600}
+                                        height={50}
+                                        alt="template bottom"
+                                        src="/static/images/template/bottom.png"
+                                        layout="fixed"
+                                    />
+
+                                    <RectangleLogo />
+
+                                    <div style={{ fontSize: "12px", marginTop: "10px" }}>
+                                        Pour vous désabonner{" "}
+                                        <StyledLink
+                                            color="#096A09"
+                                            href="/homepage/unsubscribe"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            style={{ fontSize: "12px" }}
+                                        >
+                                            Cliquez ici
+                                        </StyledLink>
+                                    </div>
+                                </div>
                             }
                         </div>
-                        <GreenButton>Envoyer le Mail</GreenButton>
+
+                        <Row style={{ justifyContent: "center" }}>
+                            <GreenButton>Envoyer le Mail</GreenButton>
+                        </Row>
                     </StyledModal>
                 </>
             }
