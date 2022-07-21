c14ecb180d9c2e0338c782e242a76a77a2ea8087
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-22 01:15:58 +0200

    :art: (App) Better incrementing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,17 @@ import 'regenerator-runtime/runtime'
 import { GlobalStyle } from "../styles/global";
 import "../styles/globals.css";
 
+
 function MyApp({ Component, pageProps }) {
-  axios.defaults.baseURL = '/api'
-  
+  axios.defaults.baseURL = '/api';
+
   return (
     <>
-        <Head>
-          <meta name="viewport" content="width=device-width, user-scalable=no" />
-        </Head>
-        <GlobalStyle />
-        <Component {...pageProps} />
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
     </>
   )
 }
