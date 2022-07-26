234b63bce8231d3749acd2bd4e7fa1e9a80680cd
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-07-26 13:58:36 +0200

    :sparkles: (Mail Modal) Add modal to notify the user when mail is sended

--- components/Card/Modals/MailModal.tsx
+++ components/Card/Modals/MailModal.tsx
@@ -5,26 +5,40 @@ import ModalLogic from "./ModalLogic"
 import { StyledBackgroundModal, StyledModal } from "./style"
 import Image from 'next/image'
 import RectangleLogo from "../RectangleLogo"
-import { StyledLink } from "../../Text/style"
+import { ErrorP, StyledLink } from "../../Text/style"
 import { Row } from "../../Container/style"
 import DOMPurify from 'isomorphic-dompurify';
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
+import { useState } from "react"
 
-export default function MailModal(props: { html: any, subject: string,  recipients: any  }) {
+export default function MailModal(props: { html: any, subject: string, recipients: any }) {
     const minWidth1000 = useMediaQuery('(min-width: 1000px)');
     const { Display, Opacity, toggle } = ModalLogic();
+    const [send, setSend] = useState(false)
     const html = DOMPurify.sanitize(props.html);
-    
+    const noRecipients = !(
+        (props.recipients.Contribution || props.recipients.NoContribution) &&
+        (
+            props.recipients.OldPromotion ||
+            props.recipients.ActivePromotion ||
+            props.recipients.NewPromotion ||
+            props.recipients.Other
+        )
+    );
+
     const sendMail = async (e) => {
-      e.preventDefault();
-      await axios.post(
-        `/mail/info`,
-        {
-          subject: props.subject,
-          content: html,
-          recipients: props.recipients
-        }
-      );
+        e.preventDefault();
+        await axios.post(
+            `/mail/info`,
+            {
+                subject: props.subject,
+                content: html,
+                recipients: props.recipients
+            }
+        )
+            .then((res: AxiosResponse) => {
+                if (res.status == 200) setSend(true);
+            });
     }
 
     return (
@@ -32,93 +46,107 @@ export default function MailModal(props: { html: any, subject: string,  recipien
             <GreenButton onClick={toggle}>Pré-visualiser le Mail</GreenButton>
             {Display &&
                 <>
-                    <StyledBackgroundModal onClick={toggle} Opacity={Opacity} />
+                    <StyledBackgroundModal onClick={(e) => { toggle(e); setSend(false); }} Opacity={Opacity} />
                     <StyledModal
                         width={minWidth1000 ? "800px" : undefined}
                         Opacity={Opacity}
                         style={{ maxHeight: "90vh" }}
                     >
                         <TitleCard>Pré-visualisation du Mail</TitleCard>
-                        <div
-                            style={{
-                                width: "100%",
-                                height: "100%",
-                                overflow: "auto",
-                                borderRadius: "20px",
-                                marginBottom: "30px",
-                                background: "#E8EFEA ",
-                                paddingBottom: "20px"
-                            }}
-                        >
-                            <div style={{
-                                display: "flex",
-                                flexDirection: "column",
-                                alignItems: "center",
-                                justifyContent: "center"
-                            }}>
-                                <Image
-                                    width={600}
-                                    height={77}
-                                    src="/static/images/template/info.png"
-                                    alt="template info"
-                                    layout="fixed"
-                                />
-
-                                <div
-                                    style={{
-                                        background: "url(/static/images/template/body.png)",
-                                        backgroundSize: "contain",
-                                        width: "600px",
-                                        padding: "0 40px",
-                                    }}
-                                >
-                                    <div
-                                        dangerouslySetInnerHTML={{ __html: html || "" }}
-                                        style={{
-                                            width: "100%",
-                                            padding: "15px 30px",
-                                            fontSize: "18px"
-                                        }}
+                        {send ?
+                            <>
+                                Le Mail d&apos;information pour les utilisateurs a été envoyé <br />
+                                Tu vas recevoir une copie du mail sur ton adresse de boul&apos;s
+                            </>
+                            :
+                            <div
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                    overflow: "auto",
+                                    borderRadius: "20px",
+                                    marginBottom: "30px",
+                                    background: "#E8EFEA ",
+                                    paddingBottom: "20px"
+                                }}
+                            >
+                                <div style={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    alignItems: "center",
+                                    justifyContent: "center"
+                                }}>
+                                    <Image
+                                        width={600}
+                                        height={77}
+                                        src="/static/images/template/info.png"
+                                        alt="template info"
+                                        layout="fixed"
                                     />
 
-                                    <div style={{ textAlign: "center" }}>
-                                        Besoin d&apos;assistance ?{" "}
+                                    <div
+                                        style={{
+                                            background: "url(/static/images/template/body.png)",
+                                            backgroundSize: "contain",
+                                            width: "600px",
+                                            padding: "0 40px",
+                                        }}
+                                    >
+                                        <div
+                                            dangerouslySetInnerHTML={{ __html: html || "" }}
+                                            style={{
+                                                width: "100%",
+                                                padding: "15px 30px",
+                                                fontSize: "18px"
+                                            }}
+                                        />
+
+                                        <div style={{ textAlign: "center" }}>
+                                            Besoin d&apos;assistance ?{" "}
+                                            <StyledLink
+                                                color="#096A09"
+                                                href="mailto:contact@amnet.fr"
+                                            >
+                                                contact@amnet.fr
+                                            </StyledLink>
+                                        </div>
+                                    </div>
+
+                                    <Image
+                                        width={600}
+                                        height={50}
+                                        alt="template bottom"
+                                        src="/static/images/template/bottom.png"
+                                        layout="fixed"
+                                    />
+
+                                    <RectangleLogo />
+
+                                    <div style={{ fontSize: "12px", marginTop: "10px" }}>
+                                        Pour vous désabonner{" "}
                                         <StyledLink
                                             color="#096A09"
-                                            href="mailto:contact@amnet.fr"
+                                            href="/homepage/unsubscribe"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            style={{ fontSize: "12px" }}
                                         >
-                                            contact@amnet.fr
+                                            Cliquez ici
                                         </StyledLink>
                                     </div>
                                 </div>
-
-                                <Image
-                                    width={600}
-                                    height={50}
-                                    alt="template bottom"
-                                    src="/static/images/template/bottom.png"
-                                    layout="fixed"
-                                />
-
-                                <RectangleLogo />
-
-                                <div style={{ fontSize: "12px", marginTop: "10px" }}>
-                                    Pour vous désabonner{" "}
-                                    <StyledLink
-                                        color="#096A09"
-                                        href="/homepage/unsubscribe"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                        style={{ fontSize: "12px" }}
-                                    >
-                                        Cliquez ici
-                                    </StyledLink>
-                                </div>
                             </div>
-                        </div>
+                        }
+                        {noRecipients &&
+                            <ErrorP style={{ marginBottom: "30px" }}>
+                                Tu n&apos;as pas sélectionné assez des casses pour créer une liste de diffusion<br />
+                                C&apos;est au moins une casse dans <strong>Cotisation payée</strong><br />
+                                Et au moins une casse dans <strong>Prom&apos;s</strong>
+                            </ErrorP>
+                        }
 
                         <Row style={{ justifyContent: "center" }}>
-                            <GreenButton onClick={sendMail}>Envoyer le Mail</GreenButton>
+                            <GreenButton onClick={noRecipients ? undefined : sendMail}>Envoyer le Mail</GreenButton>
                         </Row>
                     </StyledModal>
                 </>
