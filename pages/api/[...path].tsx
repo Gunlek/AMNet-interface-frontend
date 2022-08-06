4ee32246aed4965ff0853fdc3d40f5a57bc5a2d5
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-08-06 03:04:18 +0200

    :bug: (Backend Proxy) Add return in erorr 401 of Login

--- pages/api/[...path].tsx
+++ pages/api/[...path].tsx
@@ -2,7 +2,7 @@ import httpProxy from 'http-proxy'
 import Cookies from 'cookies'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { Readable } from 'node:stream';
-const API_URL = process.env.NEXT_PUBLIC_API_HOST
+const API_URL = process.env.NEXT_PUBLIC_API_HOST;
 const proxy = httpProxy.createProxyServer()
 
 export const config = {
@@ -21,17 +21,17 @@ async function buffer(readable: Readable) {
 
 const path = (req: NextApiRequest, res: NextApiResponse) => {
     return new Promise<void>(async (resolve, reject) => {
-        const url = new URL(req.url, process.env.NEXT_PUBLIC_API_HOST);
-        const pathname = url.pathname
-        const isLogin = pathname === '/api/auth'
+        const url = new URL(req.url, API_URL);
+        const pathname = url.pathname;
+        const isLogin = pathname === '/api/auth';
         const cookies = new Cookies(req, res);
         const access_token = cookies.get('access_token');
         let checked = null;
-        req.url = req.url.replace(/^\/api/, process.env.NEXT_PUBLIC_API_HOST);
+        req.url = req.url.replace(/^\/api/, API_URL);
         req.headers.cookie = '';
 
         if (access_token) {
-            req.headers['Authorization'] = `Bearer ${access_token}`
+            req.headers['Authorization'] = `Bearer ${access_token}`;
         };
 
         if (isLogin) {
@@ -63,19 +63,17 @@ const path = (req: NextApiRequest, res: NextApiResponse) => {
                         httpOnly: true,
                         sameSite: "lax",
                         path: "/",
-                        secure: process.env.NODE_ENV !== 'development',
                         maxAge: checked ? 1000 * 3600 * 24 * 365 * 10 : 1000 * 3600
                     });
 
                     res.status(200).json({ loggedIn: true })
                     resolve()
                 } catch (err) {
-                    reject(err)
+                    res.status(401).json({ loggedIn: false })
                 }
             })
         }
-    }
-    )
+    })
 };
 
 export default path;
