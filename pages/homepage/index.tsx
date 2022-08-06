276cf0ac4b76e9b669d5b40f13d035315daa1b3b
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-08-06 02:56:22 +0200

    :dizzy: (HomePage) Add transition if user is logged

--- pages/homepage/index.tsx
+++ pages/homepage/index.tsx
@@ -60,109 +60,116 @@ export default function Homepage(props: {
       <CampusGlobalStyle padding="0 5%" />
 
       <motion.main
-        variants={variants("right", "left")}
+        variants={variants("right", props.isLogged ? "" : "left", props.isLogged ? "#E8EFEA" : null)}
         initial="hidden"
         animate="enter"
         exit="exit"
         transition={{ type: 'linear' }}
       >
-        <Row margin="20px 0" mobileMargin="30px 0" direction="column">
-          <Col6 mobileMarginBottom="30px" justify="center" mobileAlign="center">
-            <RectangleLogo color="white" easter={true} />
-          </Col6>
-          <Col6 align="end" mobileAlign="center" justify="center">
-            <ButtonLink href={props.isLogged ? "/" : "/homepage/login"} width="300px">Accéder à Mon Compte</ButtonLink>
-          </Col6>
-        </Row>
+        <motion.div
+          variants={{ exit: { opacity: 0, x: props.isLogged ? 100 : 0 } }}
+          exit="exit"
+          transition={{ type: 'linear' }}
+          style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}
+        >
+          <Row margin="20px 0" mobileMargin="30px 0" direction="column">
+            <Col6 mobileMarginBottom="30px" justify="center" mobileAlign="center">
+              <RectangleLogo color="white" easter={true} />
+            </Col6>
+            <Col6 align="end" mobileAlign="center" justify="center">
+              <ButtonLink href={props.isLogged ? "/" : "/homepage/login"} width="300px">Accéder à Mon Compte</ButtonLink>
+            </Col6>
+          </Row>
 
-        <ResponsiveRow marginBottom="30px" mobileMarginBottom="30px" style={{ flex: "10" }}>
-          <Col7 paddingRight="15px" mobileMarginBottom="30px"
-            style={{
-              paddingTop: "0px",
-              justifyContent: "space-between"
-            }}
-          >
-            <div style={{ marginBottom: "30px" }}>
-              <WhiteP style={{ marginBottom: "1.2rem" }}>
-                <span style={{ paddingLeft: "4rem" }}>L’AMNet</span> est une association gérée par les étudiants qui a pour but
-                d’administrer le réseau internet de la résidence Jacques Pagliero des Arts et Métiers du campus de Lille.
-                Elle est totalement indépendante de l’administration de la résidence ou de l’école.
-              </WhiteP>
-              <WhiteP>
-                <span style={{ paddingLeft: "4rem" }}>Votre</span> cotisation sert à améliorer l’installation ainsi
-                qu’à payer les abonnements Internet. Chaque année, 80% des
-                cotisations est utilisé directement et les 20% restants servent à
-                créer une trésorerie pour des investissements futurs.
-              </WhiteP>
-            </div>
+          <ResponsiveRow marginBottom="30px" mobileMarginBottom="30px" style={{ flex: "10" }}>
+            <Col7 paddingRight="15px" mobileMarginBottom="30px"
+              style={{
+                paddingTop: "0px",
+                justifyContent: "space-between"
+              }}
+            >
+              <div style={{ marginBottom: "30px" }}>
+                <WhiteP style={{ marginBottom: "1.2rem" }}>
+                  <span style={{ paddingLeft: "4rem" }}>L’AMNet</span> est une association gérée par les étudiants qui a pour but
+                  d’administrer le réseau internet de la résidence Jacques Pagliero des Arts et Métiers du campus de Lille.
+                  Elle est totalement indépendante de l’administration de la résidence ou de l’école.
+                </WhiteP>
+                <WhiteP>
+                  <span style={{ paddingLeft: "4rem" }}>Votre</span> cotisation sert à améliorer l’installation ainsi
+                  qu’à payer les abonnements Internet. Chaque année, 80% des
+                  cotisations est utilisé directement et les 20% restants servent à
+                  créer une trésorerie pour des investissements futurs.
+                </WhiteP>
+              </div>
 
-          </Col7>
+            </Col7>
 
-          <Col5 marginLeft="15px" style={{ justifyContent: "space-between" }}>
-            <TeamPicture Team={props.accutalTeam} promotion={props.promotion} />
-          </Col5>
-        </ResponsiveRow>
+            <Col5 marginLeft="15px" style={{ justifyContent: "space-between" }}>
+              <TeamPicture Team={props.accutalTeam} promotion={props.promotion} />
+            </Col5>
+          </ResponsiveRow>
 
-        <ResponsiveRow marginBottom="20px" mobileMarginBottom="30px">
-          <Col7 paddingRight="15px" mobileMarginBottom="30px"
-            style={{
-              paddingTop: "0px",
-              justifyContent: "space-between"
-            }}
-          >
-            <StyledCardCampus>
-              <TitleCard>Serveur Minecraft</TitleCard>
-              <ResponsiveRow direction="column-reverse">
-                <Col2
-                  style={{
-                    alignItems: "center",
-                    justifyContent: "center"
-                  }}
-                >
-                  <MinecraftContainer>
-                    <Image width={145} height={145} src="/static/logo/mc_logo.png" alt="Logo Minecraft" />
-                  </MinecraftContainer>
-                </Col2>
+          <ResponsiveRow marginBottom="20px" mobileMarginBottom="30px">
+            <Col7 paddingRight="15px" mobileMarginBottom="30px"
+              style={{
+                paddingTop: "0px",
+                justifyContent: "space-between"
+              }}
+            >
+              <StyledCardCampus>
+                <TitleCard>Serveur Minecraft</TitleCard>
+                <ResponsiveRow direction="column-reverse">
+                  <Col2
+                    style={{
+                      alignItems: "center",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <MinecraftContainer>
+                      <Image width={145} height={145} src="/static/logo/mc_logo.png" alt="Logo Minecraft" />
+                    </MinecraftContainer>
+                  </Col2>
 
-                <Col10 paddingLeft="30px">
-                  <BlackP style={{ marginBottom: "1.2rem" }}>
-                    En plus de fournir un accès internet aux résidents nous
-                    offrons une multitude de services, un serveur Minecraft :{" "}
-                    <span style={{ color: "#096A09" }}>minecraft.amnet.fr</span>{" "}
-                    et d&apos;autres que nous vous laisserons découvrir un jour ...
+                  <Col10 paddingLeft="30px">
+                    <BlackP style={{ marginBottom: "1.2rem" }}>
+                      En plus de fournir un accès internet aux résidents nous
+                      offrons une multitude de services, un serveur Minecraft :{" "}
+                      <span style={{ color: "#096A09" }}>minecraft.amnet.fr</span>{" "}
+                      et d&apos;autres que nous vous laisserons découvrir un jour ...
+                    </BlackP>
+                    <BlackP>
+                      Si vous avez des idées de services que nous pourrions proposer
+                      hésitez pas à nous <StyledLink color="#096A09" href="mailto:contact@amnet.fr">contacter</StyledLink> !
+                    </BlackP>
+                  </Col10>
+                </ResponsiveRow>
+              </StyledCardCampus>
+            </Col7>
+
+            <Col5 marginLeft="15px" style={{ justifyContent: "space-between" }}>
+              <StyledCardCampus style={{ height: "100%" }}>
+                <TitleCard>A propos</TitleCard>
+                <Column style={{ justifyContent: "space-between", height: "100%" }}>
+                  <BlackP mobileAlignTxt="center" style={{ marginBottom: "10px" }} >
+                    Projet développé et maintenu par Hard Win&apos;∫ 58Li218, Squall&apos;∫ 4Li218 et Mac Nhat&apos;∫ 47-102Li219
                   </BlackP>
-                  <BlackP>
-                    Si vous avez des idées de services que nous pourrions proposer
-                    hésitez pas à nous <StyledLink color="#096A09" href="mailto:contact@amnet.fr">contacter</StyledLink> !
-                  </BlackP>
-                </Col10>
-              </ResponsiveRow>
-            </StyledCardCampus>
-          </Col7>
 
-          <Col5 marginLeft="15px" style={{ justifyContent: "space-between" }}>
-            <StyledCardCampus style={{ height: "100%" }}>
-              <TitleCard>A propos</TitleCard>
-              <Column style={{ justifyContent: "space-between", height: "100%" }}>
-                <BlackP mobileAlignTxt="center" style={{ marginBottom: "10px" }} >
-                  Projet développé et maintenu par Hard Win&apos;∫ 58Li218, Squall&apos;∫ 4Li218 et Mac Nhat&apos;∫ 47-102Li219
-                </BlackP>
+                  <BlackText style={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "10px" }}>
+                    <span style={{ marginRight: "15px", fontSize: "1.5rem" }}>Version 2.0.1 </span><GitHub height="40px" margin="0" />
+                  </BlackText>
 
-                <BlackText style={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "10px" }}>
-                  <span style={{ marginRight: "15px", fontSize: "1.5rem" }}>Version 2.0.1 </span><GitHub height="40px" margin="0" />
-                </BlackText>
+                  <BlackText style={{ textAlign: "right" }}>
+                    Design et UI pensés avec l&apos;aide de Cou&apos;∫<span style={{ marginLeft: "1.5px" }}>&apos;</span>tal 141Li219
+                  </BlackText>
+                </Column>
+              </StyledCardCampus>
+            </Col5>
+          </ResponsiveRow>
 
-                <BlackText style={{ textAlign: "right" }}>
-                  Design et UI pensés avec l&apos;aide de Cou&apos;∫<span style={{ marginLeft: "1.5px" }}>&apos;</span>tal 141Li219
-                </BlackText>
-              </Column>
-            </StyledCardCampus>
-          </Col5>
-        </ResponsiveRow>
-
-        <Row style={{ alignItems: "end", flex: "2" }}>
-          <HelpSection mobileMarginBottom="30px" marginBottom="20px" />
-        </Row>
+          <Row style={{ alignItems: "end", flex: "2" }}>
+            <HelpSection mobileMarginBottom="30px" marginBottom="20px" />
+          </Row>
+        </motion.div>
       </motion.main>
     </>
   );
