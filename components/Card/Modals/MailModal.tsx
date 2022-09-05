22708bf4d15b7207ea02b08fa5c8b08741493723
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-09-05 04:40:52 +0200

    :children_crossing: (Mail Modal) Add error if empty mail subject

--- components/Card/Modals/MailModal.tsx
+++ components/Card/Modals/MailModal.tsx
@@ -52,102 +52,111 @@ export default function MailModal(props: { html: any, subject: string, recipient
                         Opacity={Opacity}
                         style={{ maxHeight: "90vh" }}
                     >
-                        <TitleCard>Pré-visualisation du Mail</TitleCard>
                         {send ?
                             <>
                                 Le Mail d&apos;information pour les utilisateurs a été envoyé <br />
-                                Tu vas recevoir une copie du mail sur ton adresse de boul&apos;s
+                                Tu vas en recevoir une copie du mail sur ton adresse de boul&apos;s
                             </>
                             :
-                            <div
-                                style={{
-                                    width: "100%",
-                                    height: "100%",
-                                    overflow: "auto",
-                                    borderRadius: "20px",
-                                    marginBottom: "30px",
-                                    background: "#E8EFEA ",
-                                    paddingBottom: "20px"
-                                }}
-                            >
-                                <div style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    alignItems: "center",
-                                    justifyContent: "center"
-                                }}>
-                                    <Image
-                                        width={600}
-                                        height={77}
-                                        src="/static/images/template/info.png"
-                                        alt="template info"
-                                        layout="fixed"
-                                    />
-
-                                    <div
-                                        style={{
-                                            background: "url(/static/images/template/body.png)",
-                                            backgroundSize: "contain",
-                                            width: "600px",
-                                            padding: "0 40px",
-                                        }}
-                                    >
-                                        <div
-                                            dangerouslySetInnerHTML={{ __html: html || "" }}
-                                            style={{
-                                                width: "100%",
-                                                padding: "15px 30px",
-                                                fontSize: "18px"
-                                            }}
+                            <>
+                                <TitleCard>Pré-visualisation du Mail</TitleCard>
+                                <div
+                                    style={{
+                                        width: "100%",
+                                        height: "100%",
+                                        overflow: "auto",
+                                        borderRadius: "20px",
+                                        marginBottom: "30px",
+                                        background: "#E8EFEA ",
+                                        paddingBottom: "20px"
+                                    }}
+                                >
+                                    <div style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center",
+                                        justifyContent: "center"
+                                    }}>
+                                        <Image
+                                            width={600}
+                                            height={77}
+                                            src="/static/images/template/info.png"
+                                            alt="template info"
+                                            layout="fixed"
                                         />
 
-                                        <div style={{ textAlign: "center" }}>
-                                            Besoin d&apos;assistance ?{" "}
+                                        <div
+                                            style={{
+                                                background: "url(/static/images/template/body.png)",
+                                                backgroundSize: "contain",
+                                                width: "600px",
+                                                padding: "0 40px",
+                                            }}
+                                        >
+                                            <div
+                                                dangerouslySetInnerHTML={{ __html: html || "" }}
+                                                style={{
+                                                    width: "100%",
+                                                    padding: "15px 30px",
+                                                    fontSize: "18px"
+                                                }}
+                                            />
+
+                                            <div style={{ textAlign: "center" }}>
+                                                Besoin d&apos;assistance ?{" "}
+                                                <StyledLink
+                                                    color="#096A09"
+                                                    href="mailto:contact@amnet.fr"
+                                                >
+                                                    contact@amnet.fr
+                                                </StyledLink>
+                                            </div>
+                                        </div>
+
+                                        <Image
+                                            width={600}
+                                            height={50}
+                                            alt="template bottom"
+                                            src="/static/images/template/bottom.png"
+                                            layout="fixed"
+                                        />
+
+                                        <RectangleLogo />
+
+                                        <div style={{ fontSize: "12px", marginTop: "10px" }}>
+                                            Pour vous désabonner{" "}
                                             <StyledLink
                                                 color="#096A09"
-                                                href="mailto:contact@amnet.fr"
+                                                href="/homepage/unsubscribe"
+                                                target="_blank"
+                                                rel="noreferrer"
+                                                style={{ fontSize: "12px" }}
                                             >
-                                                contact@amnet.fr
+                                                Cliquez ici
                                             </StyledLink>
                                         </div>
                                     </div>
-
-                                    <Image
-                                        width={600}
-                                        height={50}
-                                        alt="template bottom"
-                                        src="/static/images/template/bottom.png"
-                                        layout="fixed"
-                                    />
-
-                                    <RectangleLogo />
-
-                                    <div style={{ fontSize: "12px", marginTop: "10px" }}>
-                                        Pour vous désabonner{" "}
-                                        <StyledLink
-                                            color="#096A09"
-                                            href="/homepage/unsubscribe"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                            style={{ fontSize: "12px" }}
-                                        >
-                                            Cliquez ici
-                                        </StyledLink>
-                                    </div>
                                 </div>
-                            </div>
-                        }
-                        {noRecipients &&
-                            <ErrorP style={{ marginBottom: "30px" }}>
-                                Tu n&apos;as pas sélectionné assez des casses pour créer une liste de diffusion<br />
-                                C&apos;est au moins une casse dans <strong>Cotisation payée</strong><br />
-                                Et au moins une casse dans <strong>Prom&apos;s</strong>
-                            </ErrorP>
-                        }
 
-                        <Row style={{ justifyContent: "center" }}>
-                            <GreenButton onClick={noRecipients ? undefined : sendMail}>Envoyer le Mail</GreenButton>
-                        </Row>
+                                {noRecipients &&
+                                    <ErrorP style={{ marginBottom: "30px" }}>
+                                        Tu n&apos;as pas sélectionné assez des casses pour créer une liste de diffusion<br />
+                                        C&apos;est au moins une casse dans <strong>Cotisation payée</strong><br />
+                                        Et au moins une casse dans <strong>Prom&apos;s</strong>
+                                    </ErrorP>
+                                }
+
+                                {props.subject === "" &&
+                                    <ErrorP style={{ marginBottom: "30px" }}>
+                                        <strong>Attention</strong> tu n'as pas rempli l'Objet du mail
+                                    </ErrorP>
+                                }
+                                
+                                <Row style={{ justifyContent: "center" }}>
+                                    <GreenButton onClick={noRecipients ? undefined : sendMail}>Envoyer le Mail</GreenButton>
+                                </Row>
+                            </>
+                        }
                     </StyledModal>
                 </>
             }
