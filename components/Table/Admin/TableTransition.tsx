402e36fbe4b3974f2687b6efb89071e2014a203c
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-09-27 09:46:53 +0200

    :recycle: (Table Transition) Split code into several files

--- components/Table/Admin/TableTransition.tsx
+++ components/Table/Admin/TableTransition.tsx
@@ -1,8 +1,6 @@
-import { motion, AnimatePresence, LayoutGroup } from "framer-motion";
 import { useState } from "react";
-import { StyledTable, StyledHeadTr, StyledTh } from "../style";
 
-export default function useTransition(ContainerRef: any, Requests: any[]) {
+export default function useTransition() {
     const [Display, setDisplay] = useState({ active: false, declined: false, pending: true });
     const [Tab, setTab] = useState({ old: null, new: "pending" });
 
@@ -19,75 +17,3 @@ export default function useTransition(ContainerRef: any, Requests: any[]) {
 
     return { Display, Tab, handleTabChange }
 }
-
-export function AdminTable(props: {
-    children: React.ReactNode,
-    status: { new: string },
-    display: { declined: boolean },
-    type?: string
-}) {
-    const tBodyProps = {
-        as: motion.table,
-        initial: { opacity: 0 },
-        animate: { opacity: 1 },
-        exit: { opacity: 0 },
-        transition: { ease: "linear", duration: 0.5 }
-    };
-
-    return (
-        <StyledTable {...tBodyProps}>
-            <thead style={{ position: "sticky", top: "0", zIndex: "2" }}>
-                <StyledHeadTr>
-                    <StyledTh scope="col">#</StyledTh>
-                    <StyledTh scope="col">Utilisateur</StyledTh>
-                    <StyledTh scope="col">Cotisation</StyledTh>
-                    <StyledTh scope="col">Description</StyledTh>
-                    {!(props.type === "access") && <StyledTh scope="col">Détails</StyledTh>}
-                    {props.display.declined && <StyledTh scope="col">Motif du Refus</StyledTh>}
-                    {props.type === "access" &&
-                        <>
-                            <StyledTh style={{ width: "230px", textAlign: "center" }} scope="col">Adresse Mac</StyledTh>
-                            <StyledTh scope="col">Preuve</StyledTh>
-                        </>
-                    }
-                    <StyledTh scope="col"><span style={{ paddingLeft: "5px" }}>Etat</span></StyledTh>
-                    <StyledTh scope="col">
-                        <div
-                            style={{
-                                width: (props.status.new == "pending") ? "500px" : "325px",
-                                paddingLeft: "5px",
-                                transition: "width 0s linear 0.6s"
-                            }}
-                        >
-                            Actions
-                        </div>
-                    </StyledTh>
-                </StyledHeadTr>
-            </thead>
-            <tbody>
-                <AnimatePresence initial={false}>{props.children}</AnimatePresence>
-            </tbody>
-        </StyledTable >
-    )
-};
-
-export function MobileAdminTable(props: {
-    children: React.ReactNode,
-}) {
-    const tBodyProps = {
-        initial: { opacity: 0 },
-        animate: { opacity: 1 },
-        exit: { opacity: 0 },
-        transition: { ease: "linear", duration: 0.5 }
-    };
-
-    return (
-        <motion.div {...tBodyProps}>
-            <LayoutGroup>
-                <AnimatePresence initial={false}>
-                    {props.children}
-                </AnimatePresence>
-            </LayoutGroup>
-        </motion.div>
-    )
-};
