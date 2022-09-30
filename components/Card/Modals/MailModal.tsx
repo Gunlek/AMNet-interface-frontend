5f4eab064b71241f0265258d3470dac84c5376a9
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-09-30 10:08:10 +0200

    :iphone: (Mail Modal) Create a responsive layout

--- components/Card/Modals/MailModal.tsx
+++ components/Card/Modals/MailModal.tsx
@@ -16,6 +16,8 @@ export default function MailModal(props: { html: any, subject: string, recipient
     const { Display, toggle } = ModalLogic();
     const [send, setSend] = useState(false)
     const html = DOMPurify.sanitize(props.html);
+    const minWidth740 = useMediaQuery('(min-width: 740px)');
+
     const noRecipients = !(
         (props.recipients.Contribution || props.recipients.NoContribution) &&
         (
@@ -52,119 +54,119 @@ export default function MailModal(props: { html: any, subject: string, recipient
         <>
             <GreenButton mobileWidth="100%" onClick={toggle}>Pré-visualiser le Mail</GreenButton>
             <DefaultModal
-                style={{ width: "800px", maxHeight: "90vh" }}
+                style={{ width: "800px" }}
                 Display={Display}
                 toggle={(e) => { toggle(e); setSend(false); }}
             >
-                <AnimatePresence initial={false} exitBeforeEnter>
-                    {send ?
-                        <motion.div key="message" {...divProps} layout>
-                            Le Mail d&apos;information pour les utilisateurs a été envoyé <br />
-                            Tu vas en recevoir une copie du mail sur ton adresse de boul&apos;s
-                        </motion.div>
-                        :
-                        <motion.div key="mail" {...divProps} layout>
-                            <TitleCard>Pré-visualisation du Mail</TitleCard>
-                            <div
-                                style={{
-                                    width: "100%",
-                                    height: "100%",
-                                    overflow: "auto",
-                                    borderRadius: "20px",
-                                    marginBottom: "30px",
-                                    background: "#E8EFEA ",
-                                    paddingBottom: "20px"
-                                }}
-                            >
-                                <div style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    alignItems: "center",
-                                    justifyContent: "center"
-                                }}>
-                                    <Image
-                                        width={600}
-                                        height={77}
-                                        src="/static/images/template/info.png"
-                                        alt="template info"
-                                        layout="fixed"
-                                    />
-
-                                    <div
-                                        style={{
-                                            background: "url(/static/images/template/body.png)",
-                                            backgroundSize: "contain",
-                                            width: "600px",
-                                            padding: "0 40px",
-                                        }}
-                                    >
-                                        <div
-                                            dangerouslySetInnerHTML={{ __html: html || "" }}
-                                            style={{
-                                                width: "100%",
-                                                padding: "15px 30px",
-                                                fontSize: "18px"
-                                            }}
+                <div style={{height: "100%", width: "100%", overflow: "auto"}}>
+                    <AnimatePresence initial={false} exitBeforeEnter>
+                        {send ?
+                            <motion.div key="message" {...divProps} layout>
+                                Le Mail d&apos;information pour les utilisateurs a été envoyé <br />
+                                Tu vas en recevoir une copie du mail sur ton adresse de boul&apos;s
+                            </motion.div>
+                            :
+                            <motion.div key="mail" {...divProps} layout>
+                                <TitleCard>Pré-visualisation du Mail</TitleCard>
+                                <div
+                                    style={{
+                                        width: "100%",
+                                        height: "100%",
+                                        overflow: "auto",
+                                        borderRadius: "20px",
+                                        marginBottom: "30px",
+                                        background: "#E8EFEA ",
+                                        paddingBottom: "20px"
+                                    }}
+                                >
+                                    <div style={{
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        alignItems: "center",
+                                        justifyContent: "center"
+                                    }}>
+                                        <Image
+                                            width={600}
+                                            height={77}
+                                            src="/static/images/template/info.png"
+                                            alt="template info"
                                         />
 
-                                        <div style={{ textAlign: "center" }}>
-                                            Besoin d&apos;assistance ?{" "}
+                                        <div
+                                            style={{
+                                                background: "url(/static/images/template/body.png)",
+                                                backgroundSize: "contain",
+                                                width: minWidth740 ? "600px": "100%",
+                                                padding: "0 40px",
+                                            }}
+                                        >
+                                            <div
+                                                dangerouslySetInnerHTML={{ __html: html || "" }}
+                                                style={{
+                                                    width: "100%",
+                                                    padding: "15px 30px",
+                                                    fontSize: "18px"
+                                                }}
+                                            />
+
+                                            <div style={{ textAlign: "center" }}>
+                                                Besoin d&apos;assistance ?{" "}
+                                                <StyledLink
+                                                    color="#096A09"
+                                                    href="mailto:contact@amnet.fr"
+                                                >
+                                                    contact@amnet.fr
+                                                </StyledLink>
+                                            </div>
+                                        </div>
+
+                                        <Image
+                                            width={600}
+                                            height={50}
+                                            alt="template bottom"
+                                            src="/static/images/template/bottom.png"
+                                        />
+
+                                        <RectangleLogo />
+
+                                        <div style={{ fontSize: "12px", marginTop: "10px" }}>
+                                            Pour vous désabonner{" "}
                                             <StyledLink
                                                 color="#096A09"
-                                                href="mailto:contact@amnet.fr"
+                                                href="/homepage/unsubscribe"
+                                                target="_blank"
+                                                rel="noreferrer"
+                                                style={{ fontSize: "12px" }}
                                             >
-                                                contact@amnet.fr
+                                                Cliquez ici
                                             </StyledLink>
                                         </div>
                                     </div>
-
-                                    <Image
-                                        width={600}
-                                        height={50}
-                                        alt="template bottom"
-                                        src="/static/images/template/bottom.png"
-                                        layout="fixed"
-                                    />
-
-                                    <RectangleLogo />
-
-                                    <div style={{ fontSize: "12px", marginTop: "10px" }}>
-                                        Pour vous désabonner{" "}
-                                        <StyledLink
-                                            color="#096A09"
-                                            href="/homepage/unsubscribe"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                            style={{ fontSize: "12px" }}
-                                        >
-                                            Cliquez ici
-                                        </StyledLink>
-                                    </div>
                                 </div>
-                            </div>
 
-                            {noRecipients &&
-                                <ErrorPNoFixed style={{ marginBottom: "30px" }}>
-                                    Tu n&apos;as pas sélectionné assez des casses pour créer une liste de diffusion<br />
-                                    C&apos;est au moins une casse dans <strong>Cotisation payée</strong><br />
-                                    Et au moins une casse dans <strong>Prom&apos;s</strong>
-                                </ErrorPNoFixed>
-                            }
+                                {noRecipients &&
+                                    <ErrorPNoFixed style={{ marginBottom: "30px" }}>
+                                        Tu n&apos;as pas sélectionné assez des casses pour créer une liste de diffusion<br />
+                                        C&apos;est au moins une casse dans <strong>Cotisation payée</strong><br />
+                                        Et au moins une casse dans <strong>Prom&apos;s</strong>
+                                    </ErrorPNoFixed>
+                                }
 
-                            {props.subject == "" &&
-                                <ErrorPNoFixed style={{ marginBottom: "30px" }}>
-                                    <strong>Attention</strong> tu n&apos;as pas rempli l&apos;Objet du mail
-                                </ErrorPNoFixed>
-                            }
+                                {props.subject == "" &&
+                                    <ErrorPNoFixed style={{ marginBottom: "30px" }}>
+                                        <strong>Attention</strong> tu n&apos;as pas rempli l&apos;Objet du mail
+                                    </ErrorPNoFixed>
+                                }
 
-                            <Row style={{ justifyContent: "center" }}>
-                                <GreenButton onClick={noRecipients || props.subject == "" ? undefined : sendMail}>
-                                    Envoyer le Mail
-                                </GreenButton>
-                            </Row>
-                        </motion.div>
-                    }
-                </AnimatePresence>
+                                <Row style={{ justifyContent: "center" }}>
+                                    <GreenButton onClick={noRecipients || props.subject == "" ? undefined : sendMail}>
+                                        Envoyer le Mail
+                                    </GreenButton>
+                                </Row>
+                            </motion.div>
+                        }
+                    </AnimatePresence>
+                </div>
             </DefaultModal>
 
         </>
