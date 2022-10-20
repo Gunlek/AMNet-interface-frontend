54567fbfb969a501408d793b1f2cac4077cc01fd
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-10-20 15:31:21 +0200

    :recycle: (Google script) replace div by react fragment

--- components/Script/GoogleScript.tsx
+++ components/Script/GoogleScript.tsx
@@ -2,7 +2,7 @@ import Script from "next/script";
 
 export default function GoogleScript() {
     return (
-        <div>
+        <>
             <Script
                 src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
                 strategy="afterInteractive"
@@ -16,6 +16,6 @@ export default function GoogleScript() {
                     gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
                 `}
             </Script>
-        </div>
+        </>
     )
 }
