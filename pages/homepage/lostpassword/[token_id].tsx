9f60450e6203c88bfda681b5933ea82294990161
ArgillaSeyshiro <80204786+ArgillaSeyshiro@users.noreply.github.com>
2022-12-08 04:01:19 +0100

    :children_crossing: (LostPassword) Add required to input and animation

--- pages/homepage/lostpassword/[token_id].tsx
+++ pages/homepage/lostpassword/[token_id].tsx
@@ -10,13 +10,14 @@ import RectangleLogo from "../../../components/Card/RectangleLogo";
 import { StyledCardCampus } from "../../../components/Card/style";
 import { Row } from "../../../components/Container/style";
 import { StyledInput, StyledInputLabel } from "../../../components/Input/style";
-import { ErrorP } from "../../../components/Text/style";
 import { variants } from "../../../components/Utils/animation-variants";
 import dynamic from "next/dynamic";
 const Modal = dynamic(() => import("../../../components/Card/Modals/Modal"));
 const PasswordInput = dynamic(() => import("../../../components/Input/PasswordInput"), {
-  loading: () => <StyledInput/>
+  loading: () => <StyledInput />
 });
+const ErrorPNoFixed = dynamic<any>(() => import("../../../components/Text/style").then((mod) => mod.ErrorPNoFixed));
+const AnimatePresence = dynamic(() => import("framer-motion").then((mod) => mod.AnimatePresence));
 
 export async function getServerSideProps({ params }) {
   const name = await (await axios.get(`${process.env.NEXT_PUBLIC_API_HOST}/user/password/${params.token_id}`)).data
@@ -34,8 +35,9 @@ export async function getServerSideProps({ params }) {
 export default function ResetPassword(props: { name: string, token: string }) {
   const [form, setForm] = useState({ password1: "", password2: "" });
   const [error, setError] = useState(false);
+  const [blur, setBlur] = useState(false);
   const [show, setShow] = useState(false);
-  
+
   const changePassword = async (e) => {
     e.preventDefault();
     if (form.password1 === form.password2 && form.password1 !== "") {
@@ -49,18 +51,14 @@ export default function ResetPassword(props: { name: string, token: string }) {
     }
   }
 
-  const onBlur = () => {
-    setError(form.password1 !== form.password2)
-  }
-
   return (
     <>
       <Head>
         <title>Réinitialisation &bull; AMNet</title>
       </Head>
       <CampusGlobalStyle />
-      <CampusBackground/>
-      
+      <CampusBackground />
+
       <Modal show={show} style={{ width: "450px" }}>
         Votre mot de passe a été mis à jour
 
@@ -95,17 +93,35 @@ export default function ResetPassword(props: { name: string, token: string }) {
 
               <div style={{ marginBottom: "20px" }}>
                 <StyledInputLabel htmlFor="user_password">Nouveau mot de passe</StyledInputLabel>
-                <PasswordInput id="user_password" onChange={(e) => { setForm({ password1: e.target.value, password2: form.password2 }) }} />
+                <PasswordInput
+                  id="user_password"
+                  onChange={(e) => { setForm({ password1: e.target.value, password2: form.password2 }); setError(e.target.value !== form.password2); }}
+                  required
+                />
               </div>
 
               <div style={{ marginBottom: "20px" }}>
                 <StyledInputLabel htmlFor="user_password_2">Confirmez votre mot de passe</StyledInputLabel>
-                <PasswordInput id="user_password_2" onChange={(e) => { setForm({ password1: form.password1, password2: e.target.value }) }} onBlur={onBlur} />
-                {error &&
-                  <ErrorP>
-                    Les mots de passe saisis ne sont pas identiques !
-                  </ErrorP>
-                }
+                <PasswordInput
+                  id="user_password_2"
+                  onChange={(e) => { setForm({ password1: form.password1, password2: e.target.value }); setError(form.password1 !== e.target.value); }}
+                  onBlur={() => { setBlur(true) }}
+                  required
+                />
+
+                <AnimatePresence>
+                  {error && blur &&
+                    <ErrorPNoFixed
+                      as={motion.p}
+                      initial={{ height: 0, opacity: 0 }}
+                      animate={{ height: "auto", opacity: 1 }}
+                      exit={{ height: 0, opacity: 0 }}
+                      transition={{ ease: "linear", duration: 0.2 }}
+                    >
+                      Les mots de passe saisis ne sont pas identiques !
+                    </ErrorPNoFixed>
+                  }
+                </AnimatePresence>
               </div>
 
               <Row style={{ justifyContent: "center" }}>
